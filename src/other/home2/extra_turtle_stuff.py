fe0d3cade1c83f5f5bc25a1edbefcd700edb1f0f
Jonah Makowski <jonah@makowski.ca>
2021-11-28 08:41:31 -0500

    Created new home2 folder
    We did this becasue of an issue on dziadek's end

---
+++ src/other/home2/extra_turtle_stuff.py
@@ -0,0 +1,16 @@
+def draw_hex(sides, length_of_sides, t):
+    for i in range(sides):
+        t.forward(length_of_sides)
+        t.right(360 / sides)
+
+def turn(length, turn, left_or_right, t):
+    if left_or_right == 'right':
+        for i in range(length):
+            t.forward(1)
+            t.right(turn)
+    elif left_or_right == 'left':
+        for i in range(length):
+            t.forward(1)
+            t.left(turn)
+    else:
+        print('you have an error with the fuction "turn"')
---
+++ src/other/home2/google_photos_icon.py
@@ -0,0 +1,29 @@
+import turtle
+
+t = turtle.Turtle()
+
+colors = ['yellow','green','blue','red']
+curve = 3
+t.speed(0)
+color = 0
+heading = 90
+
+for i in range(len(colors)):
+    t.setheading(heading)
+    t.fillcolor(colors[color])
+    t.begin_fill()
+    t.goto(0, 0)
+    t.color(colors[i])
+    for i in range(50):
+        t.left(curve)
+        t.forward(5)
+    t.goto(0, 0)
+    t.end_fill()
+    color += 1
+    t.right(1)
+    if heading == 270:
+        heading = 0
+    else:
+        heading += 90
+t.penup()
+t.goto(1000, 1000)
