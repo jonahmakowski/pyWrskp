091c0cadf0c16d087ba478241f17261ec22b871b
Jonah Makowski <jonah@makowski.ca>
2021-11-29 18:18:30 -0500

    Message bot with custom input created
    Message bot sends what you type to #bots_pywrskp & #general on slack

--- src/custom_slack_bot/bot.py
+++ src/custom_slack_bot/bot.py
@@ -2,18 +2,22 @@ import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from flask import Flask
-from slackeventsapi import SlackEventAdapter
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/events',app)
-
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 
-client.chat_postMessage(channel='#bots_pywrskp', text='Hello World!')
+user = input('What is your name?')
+print('What would you like to send?')
+mess = ''
+while True:
+    message = input()
+    if message == ' ':
+        break
+    message += '\n'
+    mess += message
+final_message = ('{}\n\nSent by: {}'.format(mess, user))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+client.chat_postMessage(channel='#bots_pywrskp', text=final_message)
+client.chat_postMessage(channel='#general', text=final_message)
