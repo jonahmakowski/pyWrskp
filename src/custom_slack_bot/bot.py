ce746fc569c72fe68bb326850e81f8e2fdb849e1
Jonah Makowski <jonah@makowski.ca>
2021-11-29 17:27:14 -0500

    Newest update on the slack bot, but I can not keep going, becasue my chromebook does not support the software needed

--- src/custom_slack_bot/bot.py
+++ src/custom_slack_bot/bot.py
@@ -2,10 +2,18 @@ import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from flask import Flask
+from slackeventsapi import SlackEventAdapter
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
+app = Flask(__name__)
+slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/events',app)
+
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 
 client.chat_postMessage(channel='#bots_pywrskp', text='Hello World!')
+
+if __name__ == '__main__':
+    app.run(debug=True)
