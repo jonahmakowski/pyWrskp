fd4282cec7e957eb5a23488203fd9b559f66847b
Jonah Makowski <jonah@makowski.ca>
2021-12-10 16:16:30 -0500

    New coder
    Notes:
    - This coder works (I think), but could use more work

---
+++ src/coder-decoder/coder.py
@@ -0,0 +1,50 @@
+class coder_decoder:
+    def __init__(self, message=None, key=None, code_decode=None):
+        self.abcs = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+        if message == None:
+            message = input('What is your message?\n')
+        if key == None:
+            key = int(input('what is the key?\n'))
+        if code_decode == None:
+            code_decode = input('Would you like to code or decode (code/decode)\n')
+        
+        if key > len(self.abcs):
+            key = len(self.abcs) - 1
+                    
+        self.message = message.upper()
+        self.key = key
+        self.code_decode = code_decode
+        self.finished = ''
+        if self.code_decode == 'code':
+            self.code()
+        elif self.code_decode == 'decode':
+            self.decode()
+        else:
+            print('imporoper code/decode')
+            exit()
+    def code(self):
+        message = list(self.message)
+        new_message = []
+        new_message_str = ''
+        not_hit = 0
+        
+        for i in range(len(message)):
+            for l in range(len(self.abcs)):
+                not_hit += 1
+                if self.abcs[l] == message[i]:
+                    new_message.append(self.abcs[l + self.key])
+                    not_hit = 0
+                if not_hit == len(self.abcs):
+                    new_message.append(message[i])
+        
+        for item in new_message:
+            new_message_str += item
+        
+        print('your coded message is {}'.format(new_message_str))
+        print('give your friend this info:')
+        print('message: {}\ncode: {}'.format(new_message_str, self.key))
+        
+    def decode(self):
+        pass
+
+code = coder_decoder()
