a9da292118735b4105caa0e329b7691498e736ef
Jonah Makowski <jonah@makowski.ca>
2021-12-12 09:48:43 -0500

    new print_ifo var

--- src/coder-decoder/coder.py
+++ src/coder-decoder/coder.py
@@ -1,13 +1,14 @@
 class coder_decoder:
-    def __init__(self, message=None, key=None, code_decode=None):
+    def __init__(self, message=None, key=None, code_decode=None, print_info=True):
         self.abcs = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' ', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '.', ',', '']
-        if message == None:
-            message = input('What is your message?\n')
-        if key == None:
-            key = int(input('what is the key? (from 1 to {})\n'.format(len(self.abcs))))
-        if code_decode == None:
-            code_decode = input('Would you like to code or decode (code/decode)\n')
-        
+        if print_info:
+            if message == None:
+                message = input('What is your message?\n')
+            if key == None:
+                key = int(input('what is the key? (from 1 to {})\n'.format(len(self.abcs))))
+            if code_decode == None:
+                code_decode = input('Would you like to code or decode (code/decode)\n')
+            
         if key > len(self.abcs):
             key = len(self.abcs) - 1
                     
@@ -15,13 +16,15 @@ class coder_decoder:
         self.key = key
         self.code_decode = code_decode
         self.finished = ''
+        self.print_info = print_info
         if self.code_decode == 'code':
             self.code()
         elif self.code_decode == 'decode':
             self.decode()
         else:
-            print('imporoper code/decode')
-            exit()
+            print('Incorrect code/decode')
+            print('exiting code')
+            return 'Error: incorrect code/decode'
     def code(self):
         message = list(self.message)
         new_message = []
@@ -37,9 +40,11 @@ class coder_decoder:
         for item in new_message:
             new_message_str += item
         
-        print('your coded message is {}'.format(new_message_str))
-        print('give your friend this info:')
-        print('message: {}\ncode: {}'.format(new_message_str, self.key))
+        if self.print_info:
+            print('your coded message is {}'.format(new_message_str))
+            print('give your friend this info:')
+            print('message: {}\ncode: {}'.format(new_message_str, self.key))
+        return new_message_str
         
     def decode(self):
         message = list(self.message)
@@ -54,6 +59,8 @@ class coder_decoder:
         for item in new_message:
             new_message_str += item
         
-        print('your decoded message is {}'.format(new_message_str))
+        if self.print_info:
+            print('your decoded message is {}'.format(new_message_str))
+        return new_message_str
 
 code = coder_decoder()
