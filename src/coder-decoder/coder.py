460b399ff5a03d6692a98990782925233d577867
Jonah Makowski <jonah@makowski.ca>
2021-12-12 10:36:03 -0500

    changed to force with limits of the key

--- src/coder-decoder/coder.py
+++ src/coder-decoder/coder.py
@@ -1,16 +1,18 @@
 class coder_decoder:
     def __init__(self, message=None, key=None, code_decode=None, print_info=True):
-        self.abcs = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' ', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '.', ',', '']
+        self.abcs = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' ', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '.', ',']
         if print_info:
             if message == None:
                 message = input('What is your message?\n')
             if key == None:
-                key = int(input('what is the key? (from 1 to {})\n'.format(len(self.abcs))))
+                while True:
+                    key = int(input('what is the key? (from 1 to {})\n'.format(len(self.abcs))))
+                    if key >= 1 and key < len(self.abcs):
+                        break
+                    else:
+                        print('inccorrect number')
             if code_decode == None:
                 code_decode = input('Would you like to code or decode (code/decode)\n')
-            
-        if key > len(self.abcs):
-            key = len(self.abcs) - 1
                     
         self.message = message.upper()
         self.key = key
@@ -24,7 +26,6 @@ class coder_decoder:
         else:
             print('Incorrect code/decode')
             print('exiting code')
-            return 'Error: incorrect code/decode'
     def code(self):
         message = list(self.message)
         new_message = []
