e5c4bb4bbde471b8612333a01beb0f76e53e890b
Jonah Makowski <jonah@makowski.ca>
2021-12-13 17:03:46 -0500

    Fixed some of the things I wanted
    Stuff I fixed:
    	*Ask if you are ok with negitive numbers for expample: 14 - 15 (= -1), I want to do this by making a var that will make this into 15 - 14
    	*Ask if you are ok with decimal numbers for example: 15 / 4 (= 3.75), so that this does not show up
    
    like I said in the last commit

--- src/math-game/game_extras.py
+++ src/math-game/game_extras.py
@@ -6,10 +6,20 @@ def add(max_num, min_num):
     a = num1 + num2
     return num1, num2, a
 
-def sub(max_num, min_num):
+def sub(max_num, min_num, neg):
     num1 = randint(max_num, min_num)
     num2 = randint(max_num, min_num)
     a = num1 - num2
+    if neg == 'n' and a < 0:
+        numq = num1
+        numw = num2
+        
+        num1 = numw
+        num2 = numq
+        
+        a = num1 - num2
+        if neg == 'n' and a < 0:
+            a = sub(max_num, min_num, neg)
     return num1, num2, a
 
 def multi(max_num, min_num):
@@ -18,8 +28,10 @@ def multi(max_num, min_num):
     a = num1 * num2
     return num1, num2, a
 
-def div(max_num, min_num):
+def div(max_num, min_num, dec):
     num1 = randint(max_num, min_num)
     num2 = randint(max_num, min_num)
     a = num1 / num2
+    if dec == 'n' and isinstance(a, float):
+        a = div(max_num, min_num, dec)
     return num1, num2, a
