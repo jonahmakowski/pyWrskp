366440ed2a5ae4e70b49caddca45908ba1e72290
Jonah Makowski <jonah@makowski.ca>
2021-12-13 16:48:49 -0500

    set up game
    Stuff to add:
    	*Ask if you are ok with negitive numbers for expample: 14 - 15 (= -1), I want to do this by making a var that will make this into 15 - 14
    	*Ask if you are ok with decimal numbers for example: 15 / 4 (= 3.75), so that this does not show up

--- src/math-game/game.py
+++ src/math-game/game.py
@@ -1,1 +1,90 @@
+import game_extras
+from random import randint as r
 
+class Game:
+    def __init__(self):
+        self.max = int(input('What would you like the max of the nums\n'))
+        self.min = int(input('What would you like the min of the nums\n'))
+        self.money = 0
+        self.wrong = 0
+        self.correct = 0
+        
+        while self.wrong <= 20:
+            option = r(1, 4)
+            if option == 1:
+                self.add()
+            elif option == 2:
+                self.sub()
+            elif option == 3:
+                self.multi()
+            elif option == 4:
+                self.div
+        self.over()
+    
+    def add(self):
+        num1, num2, a = game_extras.add(self.max, self.min)
+        ask = int(input('What is {} + {}?\n'.format(num1, num2)))
+        print(num1, num2, a, ask)
+        if ask == a:
+            self.correct += 1
+            self.money += 1
+            print('You have {} correct'.format(self.correct))
+            print('You have {} money'.format(self.money))
+        elif ask != a:
+            self.wrong += 1
+            self.money -= 10
+            print('you lost 10 money, you now have {} money'.format(self.money))
+            print('you got {} wrong so far'.format(self.wrong))
+    
+    def sub(self):
+        num1, num2, a = game_extras.sub(self.max, self.min)
+        ask = int(input('What is {} - {}?\n'.format(num1, num2)))
+        print(num1, num2, a, ask)
+        if ask == a:
+            self.correct += 1
+            self.money += 7.5
+            print('You have {} correct'.format(self.correct))
+            print('You have {} money'.format(self.money))
+        elif ask != a:
+            self.wrong += 1
+            self.money -= 7.5
+            print('you lost 7.5 money, you now have {} money'.format(self.money))
+            print('you got {} wrong so far'.format(self.wrong))
+    
+    def multi(self):
+        num1, num2, a = game_extras.multi(self.max, self.min)
+        ask = int(input('What is {} * {}?\n'.format(num1, num2)))
+        print(num1, num2, a, ask)
+        if ask == a:
+            self.correct += 1
+            self.money += 5
+            print('You have {} correct'.format(self.correct))
+            print('You have {} money'.format(self.money))
+        elif ask != a:
+            self.wrong += 1
+            self.money -= 5
+            print('you lost 5 money, you now have {} money'.format(self.money))
+            print('you got {} wrong so far'.format(self.wrong))
+    
+    def div(self):
+        num1, num2, a = game_extras.div(self.max, self.min)
+        ask = int(input('What is {} * {}?\n'.format(num1, num2)))
+        print(num1, num2, a, ask)
+        if ask == a:
+            self.correct += 1
+            self.money += 2.5
+            print('You have {} correct'.format(self.correct))
+            print('You have {} money'.format(self.money))
+        elif ask != a:
+            self.wrong += 1
+            self.money -= 2.5
+            print('you lost 2.5 money, you now have {} money'.format(self.money))
+            print('you got {} wrong so far'.format(self.wrong))
+    
+    def over(self):
+        print('GAME OVER')
+        print('Wrong:\t{}'.format(self.wrong))
+        print('Correct:\t{}'.format(self.correct))
+        print('Money:\t{}'.format(self.money))
+
+game = Game()
--- src/math-game/game_extras.py
+++ src/math-game/game_extras.py
@@ -1,1 +1,25 @@
+from random import randint
 
+def add(max_num, min_num):
+    num1 = randint(max_num, min_num)
+    num2 = randint(max_num, min_num)
+    a = num1 + num2
+    return num1, num2, a
+
+def sub(max_num, min_num):
+    num1 = randint(max_num, min_num)
+    num2 = randint(max_num, min_num)
+    a = num1 - num2
+    return num1, num2, a
+
+def multi(max_num, min_num):
+    num1 = randint(max_num, min_num)
+    num2 = randint(max_num, min_num)
+    a = num1 * num2
+    return num1, num2, a
+
+def div(max_num, min_num):
+    num1 = randint(max_num, min_num)
+    num2 = randint(max_num, min_num)
+    a = num1 / num2
+    return num1, num2, a
