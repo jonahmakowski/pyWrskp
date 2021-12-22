88df9a3e51add9305ee6c191451c9100fdc9c2ef
Jonah Makowski <jonah@makowski.ca>
2021-12-22 16:57:32 -0500

    Made a docs file
    This docs file saves strings to your laptop

---
+++ src/docs/docs.py
@@ -0,0 +1,36 @@
+class docs:
+    def __init__(self):
+        self.file_name = input('What is the file name?')
+    
+    def read(self):
+        file = open(self.file_name, 'r')
+        print(file.read())
+        return file
+    
+    def create(self, pr=True, add=None):
+        text = ''
+        file = open(self.file_name, 'w')
+        if pr:
+            print('What would you like to write in your file')
+        while True:
+            temp_text = input('')
+            if temp_text == ' ':
+                break
+            ttemp_text = temp_text + '\n'
+            text += ttemp_text
+        if add != None:
+            text += add.read()
+        file.write(text)
+    
+    def add(self):
+        file = self.read()
+        self.create(pr=False, add=file)
+
+doc = docs()
+info = input("Would you like read, create, or add (for add, you need to have a file and you can't remove old sections)")
+if info == 'add':
+    doc.add()
+elif info == 'create':
+    doc.create()
+elif info == 'read':
+    doc.read()
