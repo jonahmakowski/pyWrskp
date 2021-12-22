61180cc51197201cf018948c6c40abf295819f7e
Jonah Makowski <jonah@makowski.ca>
2021-12-22 17:27:01 -0500

    Update way this works

--- src/coder-decoder/coder.py
+++ src/coder-decoder/coder.py
@@ -1,9 +1,9 @@
 class coderDecoder:
-    def __init__(self, message, key, print_info=True):
+    def __init__(self, print_info=True):
         self.abcs = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' ', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '.', ',', '[', ']', '{', '}', ':', ';', "'", '"']
         self.finished = ''
         self.print_info = print_info
-    def add_vars(self, message, key):
+    def add_vars(self, message, key, code=None):
         self.message = message
         self.key = key
     def code(self):
--- src/coder-decoder/coder_run.py
+++ src/coder-decoder/coder_run.py
@@ -4,8 +4,8 @@ code = coderDecoder()
 
 message = input('What is your message?\n')
 while True:
-    key = int(input('what is the key? (from 1 to {})\n'.format(len(info.abcs))))
-    if key >= 1 and key < len(info.abcs):
+    key = int(input('what is the key? (from 1 to {})\n'.format(len(code.abcs))))
+    if key >= 1 and key < len(code.abcs):
         break
     else:
         print('inccorrect number')
