29172745c3a247f9a9142b5a89ee2b089348cc88
jonahmakowski <jonah@makowski.ca>
2021-12-29 10:43:28 -0500

    fixed errors in calculator
    - Warnings fixed (two left about the except section)
    - Comment added
    - print statement added

--- src/other/home/caculator.py
+++ src/other/home/caculator.py
@@ -1,30 +1,36 @@
-# This is a simple caculator in the shell
+# This is a simple calculator in the shell
 
-def use(num1, num2, type):
-        try:
-            if type == '+':
-                a = num1 + num2
-                q = '{} + {}'.format(num1, num2)
-            elif type == '-':
-                a = num1 - num2
-                q = '{} - {}'.format(num1, num2)
-            elif type == '*':
-                a = num1 * num2
-                q = '{} * {}'.format(num1, num2)
-            elif type == '/':
-                a = num1 / num2
-                q = '{} / {}'.format(num1, num2)
-            elif type == '**':
-                a = num1 ** num2
-                q = '{} ** {}'.format(num1, num2)
-            elif type == '^':
-                from math import sqrt
-                a = sqrt(num1)
-                q = '√{}'.format(num1)
-            else:
-                a = "ISSUE CODE CAN NOT FIND NUMBERS NESSARY, TALK TO THE OWNER OF THIS WEBSITE"
-        except:
-            q = "ISSUE"
-            a = "ISSUE"
-        return q, a
-use(input('What is the first number?'), input('What is the type?'), input('what is the other number?'))
+def use(num1, t, num2):
+    num1 = int(num1)
+    num2 = int(num2)
+    try:
+        if t == '+':
+            a = num1 + num2
+            q = '{} + {}'.format(num1, num2)
+        elif t == '-':
+            a = num1 - num2
+            q = '{} - {}'.format(num1, num2)
+        elif t == '*':
+            a = num1 * num2
+            q = '{} * {}'.format(num1, num2)
+        elif t == '/':
+            a = num1 / num2
+            q = '{} / {}'.format(num1, num2)
+        elif t == '**':
+            a = num1 ** num2
+            q = '{} ** {}'.format(num1, num2)
+        elif t == '^':
+            from math import sqrt
+            a = sqrt(num1)
+            q = '√{}'.format(num1)
+        else:
+            a = "ISSUE CODE CAN NOT FIND NUMBERS NECESSARY, TALK TO THE OWNER OF THIS WEBSITE"
+            q = a
+    except:
+        q = "ISSUE"
+        a = "ISSUE"
+    return q, a
+
+
+c, d = use(input('What is the first number?'), input('What is the type?'), input('what is the other number?'))
+print(c + ' = ' + str(d))
