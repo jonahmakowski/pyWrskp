60aa93433d4363cab2a85787966a8839ab36c1d3
jonahmakowski <jonah@makowski.ca>
2021-12-30 09:12:15 -0500

    Fixed warnings on routes.py
    Not all of the warnings have been fixed, but I changed the easy ones that you can just remove or add a space or new link.

--- src/web21/app/routes.py
+++ src/web21/app/routes.py
@@ -6,7 +6,7 @@ import os
 pyWrskpLoc = ''
 
 try:
-   pyWrskpLoc = os.environ["PYWRKSP"]
+    pyWrskpLoc = os.environ["PYWRKSP"]
    
 except KeyError:
     pyWrkspLoc = os.environ["HOME"] + input('Since you do not have the PYWRSKP env var \nPlease enter the pwd for the pyWrskp repo not including the "home" section')
@@ -14,45 +14,48 @@ User = ''
 
 blogPosts_list = [
     {
-        'creator':{'username':'Jonah', 'user':'Jonah'},
-        'content':'Why this website is better then the other',
-        'info':'It is home made by me! It also has many pages, unlike the first website I made!',
-        'link':'/post/Jonah'
+        'creator': {'username': 'Jonah', 'user': 'Jonah'},
+        'content': 'Why this website is better then the other',
+        'info': 'It is home made by me! It also has many pages, unlike the first website I made!',
+        'link': '/post/Jonah'
     },
     {
-        'creator':{'username':"Mr. Payne's 4/5 class 2020-21", 'user':"Mr.Payne's+45+class+2020-21"},
-        'content':'Why in-person school is better then the online version',
-        'info':"In-person school is sooooooooooo much better than online school becuase when we are in person we can chat, and eat together (note this was not written by Mr.Payne's class, it was written by Jonah)",
-        'link':"/post/Mr.Payne's+45+class"
+        'creator': {'username': "Mr. Payne's 4/5 class 2020-21", 'user': "Mr.Payne's+45+class+2020-21"},
+        'content': 'Why in-person school is better then the online version',
+        'info': "In-person school is sooooooooooo much better than online school because when we are in person we can chat, and eat together (note this was not written by Mr.Payne's class, it was written by Jonah)",
+        'link': "/post/Mr.Payne's+45+class"
         },
     {
-        'creator':{'username':'Jonah', 'user':'Jonah1'},
-        'content':'What does www stand for?',
-        'info':'What does www (like https://www...) mean? www means World Wide Web!',
-        'link':'/post/Jonah1'
+        'creator': {'username': 'Jonah', 'user': 'Jonah1'},
+        'content': 'What does www stand for?',
+        'info': 'What does www (like https://www...) mean? www means World Wide Web!',
+        'link': '/post/Jonah1'
         },
     {
-        'creator':{'username':'Jonah', 'user':'Jonah2'},
-        'content':'How to show the CEO info',
-        'info':'in your web address bar, remove "/link/Jonah2", and put "/ceo+info" instead, then press enter, and bingo, you got the CEO info!',
-        'link':'/post/Jonah2'
+        'creator': {'username': 'Jonah', 'user': 'Jonah2'},
+        'content': 'How to show the CEO info',
+        'info': 'in your web address bar, remove "/link/Jonah2", and put "/ceo+info" instead, then press enter, and bingo, you got the CEO info!',
+        'link': '/post/Jonah2'
         }]
 
-CEO = {'name':'Jonah Makowski',
-       'emails':{
-        '1':'jonah.kmjn@gmail.com',
-        '2':'jonah@makowski.ca',
-        '3':'jonah@makowski.at'},
-       'link':'https://www.youtube.com/channel/UC1ti62i-uMnBVAh9b_Pp3UA/'}
+CEO = {'name': 'Jonah Makowski',
+       'emails': {
+        '1': 'jonah.kmjn@gmail.com',
+        '2': 'jonah@makowski.ca',
+        '3': 'jonah@makowski.at'},
+       'link': 'https://www.youtube.com/channel/UC1ti62i-uMnBVAh9b_Pp3UA/'}
+
 
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/welcome', methods=['POST', 'GET'])
-def Welcome():
-    return render_template('index.html', title='Welcome', User = User)
+def welcome():
+    return render_template('index.html', title='Welcome', User=User)
+
 
 @app.route('/home', methods=['POST', 'GET'])
 def home():
-    return render_template('home.html', title='Home', User = User)
+    return render_template('home.html', title='Home', User=User)
+
 
 @app.route('/home/blogposts', methods=['POST', 'GET'])
 @app.route('/home/blogposts/', methods=['POST', 'GET'])
@@ -60,20 +63,22 @@ def home():
 def blog_posts():
     return render_template('posts.html', title='blog posts', posts=blogPosts_list)
 
+
 @app.route('/post/<creator>', methods=['POST', 'GET'])
 def post(creator):
     usr = False
-    for post in blogPosts_list:
-        if post['creator']['user'] == creator:
+    for item in blogPosts_list:
+        if item['creator']['user'] == creator:
             usr = True
             break
-    if usr == False:
+    if not usr:
         error_code = '404'
         error='{} error'.format(str(error_code))
         title = error
         return render_template('error.html', title=title, error=error)
     else:
         return render_template('Blog_outline.html', title=post['creator']['username'] + "'s blog post", post=post)
+
 
 @app.route('/home/blogposts/small', methods=['POST', 'GET'])
 @app.route('/home/blogposts/small/', methods=['POST', 'GET'])
@@ -83,34 +88,40 @@ def post(creator):
 def blog_post_small():
     return render_template('posts_small.html', title='blog posts', posts=blogPosts_list)
 
+
 @app.route('/music', methods=['POST', 'GET'])
 def music():
     return render_template('music.html', title='Music')
+
 
 @app.route('/calendar', methods=['POST', 'GET'])
 def calendar():
     return render_template('calendar.html', title='calendar')
 
+
 @app.route('/ceo+info', methods=['POST', 'GET'])
 def ceo_info():
     if request.method == "POST":
         return redirect(CEO['link'])
-    name = 'Name:         ' + CEO ['name']
+    name = 'Name:         ' + CEO['name']
     email = 'Emails:       {}, {}, {}'.format(CEO['emails']['1'], CEO['emails']['2'], CEO['emails']['3'])
     link = 'Youtube channel: ' + CEO['link']
     print(name + '\n' + email + '\n' + link)
-    return render_template('CEO_info.html', title='CEO info', name = name, email=email, link=link)
+    return render_template('CEO_info.html', title='CEO info', name=name, email=email, link=link)
+
 
 @app.route('/youtube', methods=['POST', 'GET'])
 def youtube():
     return render_template('youtube.html', title='youtube')
 
+
 @app.route('/page', methods=['POST', 'GET'])
 def page():
     return render_template('page.html', title='List of Pages | This is a list of pages on this website')
 
+
 @app.route('/cac', methods=['POST', 'GET'])
-def caculator_sender():
+def calculator():
     if request.method == "POST":
         global pyWrkspLoc
         try:
@@ -125,28 +136,29 @@ def caculator_sender():
         q, a = use(int(request.form.get("num1")),
                    num2,
                    request.form.get("type"))
-        return render_template('caculator.html', title='Caculator', a = a, q = q)
-    return render_template('caculator_redirect.html', title='Caculator Sender')
+        return render_template('caculator.html', title='Calculator', a=a, q=q)
+    return render_template('caculator_redirect.html', title='Calculator Sender')
+
 
 @app.route('/passwords', methods=['GET', 'POST'])
 def passwords():
     if request.method == "POST":
         global pyWrkspLoc
-        from random import randint
         
         sys.path.append(pyWrkspLoc + '/src/password_maker')
 
         from Creater import create
         
         password = create(request.form.get("letters"),
-                        request.form.get("numbers"),
-                        request.form.get("sc"),
-                        request.form.get("super_c"),
-                        int(request.form.get("length")),
-                        'n',
-                        False)
+                          request.form.get("numbers"),
+                          request.form.get("sc"),
+                          request.form.get("super_c"),
+                          int(request.form.get("length")),
+                          'n',
+                          False)
         return render_template('password_show.html', title='Passwords', password=password)
     return render_template('passwords.html', title='Passwords')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -156,11 +168,12 @@ def login():
         password = request.form.get("password")
         User = request.form.get("username")
         if User == 'WhiteSwine' and password == 'LOLA IS THE BEST':
-            return render_template('logged_in.html', title = 'logged in!', user = User)
+            return render_template('logged_in.html', title='logged in!', user=User)
         else:
             return '<h1>INCORRECT</h1>'
         
     return render_template('login.html', title='login')
+
 
 @app.route('/turtle', methods=['GET', 'POST'])
 def turtle():
@@ -177,6 +190,7 @@ def turtle():
         return '<h1>Done</h1> <p>if nothing is happening, you have an error</p> <a href="/">return to home page</a>'
     return render_template('turtle.html', title='Noah and me turtle')
 
+
 @app.route('/pygame/draw', methods=['GET', 'POST'])
 def draw():
     if request.method == "POST":
@@ -189,13 +203,16 @@ def draw():
         return '<h1>Done</h1> <p>if nothing is happening, you have an error</p> <a href="/">return to home page</a>'
     return render_template('pygame_draw.html', title='DRAW!')
 
+
 @app.route('/feedback', methods=['GET', 'POST'])
 def feedback():
     return render_template('feedback.html', title='Feedback')
 
+
 @app.route('/fire', methods=['GET', 'POST'])
 def fire():
     return render_template('fire.html', title='Fire')
+
 
 @app.route('/alarm', methods=['GET', 'POST'])
 def alarm():
@@ -210,9 +227,11 @@ def alarm():
         return '<h1>DING-DONG</h1> <a href="/">return to home page</a>'
     return render_template('alarm.html', title='Alarm clock!')
 
+
 @app.route('/lola', methods=['GET', 'POST'])
 def lola():
     return render_template('lola.html', title='Lola Image')
+
 
 @app.route('/code', methods=['GET', 'POST'])
 def code():
@@ -229,26 +248,34 @@ def code():
             message = coder.code()
         elif coder_decoder == 'decode':
             message = coder.decode()
+        else:
+            print('error within code')
+            print('coder_decoder did not equal code or decode, exiting')
+            message = 'error within code\ncoder_decoder did not equal code or decode, exiting'
+            exit()
         return render_template('coder_show.html', title='coder decoder show', message=message, key=key)
     return render_template('coder.html', title='coder decoder')
+
 
 @app.route('/fun', methods=['GET', 'POST'])
 def fun():
     return render_template('fun.html', title='Fun')
 
+
 @app.route('/translater', methods=['GET', 'POST'])
 def translater():
     if request.method == 'POST':
-        langage = request.form.get('language')
+        language = request.form.get('language')
         text = request.form.get('text')
         from translate import Translator
-        translator= Translator(to_lang=langage)
+        translator = Translator(to_lang=language)
         trans = translator.translate(text)
         if text != trans:
             return trans
         elif text == trans:
             return '<h1>Language not supported</h1> <p>This language you used is not one of the languages supported on this translater</p>'
     return render_template('translate.html', title='translater')
+
 
 @app.route('/christmas', methods=['GET', 'POST'])
 def christmas_tree():
