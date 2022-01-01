0bda339b18589aff71746f24f911ce61d6b543d2
jonahmakowski <jonah@makowski.ca>
2022-01-01 17:01:24 -0500

    fixed some warnings
    spelling & spaces

--- src/web21/app/routes.py
+++ src/web21/app/routes.py
@@ -322,11 +322,11 @@ def txt():
         import class_4_pt_1
         import tkinter as tk
         df_save_loc = '/home/jonah/Thonny files/TXT_files/' # change to folder name where you want auto saves as def
-        df_name = 'testing' # you can chnage def save name
+        df_name = 'testing' # you can change def save name
         root = tk.Tk()
         root.title('Text Editer')
-        root.rowconfigure(0, minsize =800, weight = 1)
-        root.columnconfigure(1, minsize =600, weight=1)
+        root.rowconfigure(0, minsize=800, weight = 1)
+        root.columnconfigure(1, minsize=600, weight=1)
         app = class_4_pt_1.Application(df_name, df_save_loc, master=root)
         app.mainloop()
         return redirect('/')
