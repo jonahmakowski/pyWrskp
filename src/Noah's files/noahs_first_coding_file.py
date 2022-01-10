5bb4b913e053760e1d14c89740d2ef6fbd4b6f53
jonahmakowski <jonah@makowski.ca>
2022-01-10 08:23:09 -0500

    Noah's coding
    Noah made a program that he really enjoied coding, might set him up on my old chromebook.

---
+++ src/Noah's files/noahs_first_coding_file.py
@@ -0,0 +1,15 @@
+name = input('What is your name?')
+if name == 'Jonah' or name == 'Noah':
+    for i in range(1000):
+        print('LOLA IS THE CUTEST NO ARGUMENT')
+
+elif name == 'Liam' or name == 'John':
+    for i in range(1000):
+        print('MOXIE IS THE CUTEST NO ARGUMENT')
+
+elif name == 'Phinn' or name == 'Orla':
+    for i in range(1000):
+        print('OSCAR IS THE CUTEST NO ARGUMENT')
+
+else:
+    print('LOLA IS THE CUTEST NO ARGUMENT')
