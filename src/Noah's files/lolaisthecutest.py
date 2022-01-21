64ab5edb1fecb86e97674ee9ed71d527ccded715
Jonah Makowski <jonah@makowski.ca>
2022-01-20 21:01:41 -0500

    more set up

---
+++ src/Noah's files/lolaisthecutest.py
@@ -0,0 +1,3 @@
+print ('lola is the cutest bulldog not moxie or oscar')
+print ('JONAH IS A MEANY')
+print ('leve me allonw')
---
+++ src/Noah's files/src/first_work/lolaisthecutest.py
@@ -0,0 +1,14 @@
+print ('lola is the cutest bulldog not moxie or oscar')
+print ('JONAH IS A MEANY')
+print ('leve me allonw')
+
+Name = input('What is your name?')
+
+if Name == 'Jonah':
+    print('shut up jonah you idiot')
+
+elif Name == 'Noah':
+    print('You are the smartest person in the world')
+
+else:
+    print('hi strange loser')
