46f073239f966c736b6d9e323794f8e0040f88cf
jonahmakowski <jonah@makowski.ca>
2022-01-22 08:42:48 -0500

    fixed some simple warnings

--- src/password_maker/Creater.py
+++ src/password_maker/Creater.py
@@ -1,16 +1,17 @@
 from random import randint
 
+
 def create(letter, num, sc, super_c, length, save, print_info):
-    
     chars = []
     letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-    scs = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '~', '`', '{', '}', '[', ']', ';', ':', '"', "'", '<', '>', '?', ',',
-           '.', '/', '|']
-    super_s_c = ['←', '↑', '→', '↓', '·', '•', '●', '–', '‽', '‖', '«', '»', '‘', '„', '✅', '❤️', '⌘', '', '⌥', '⌫', '∞', '™', '¼', '½', '¾', 'À', 'Á', 'Â', 'Ã',
-                 'Ä', 'Å', 'Æ', 'Ç', 'È', 'É', 'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï', 'Ð', 'Ñ', 'Ò', 'Ó', 'Ô', 'Õ', 'Ö', 'Ø', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'Þ', 'ß', 'æ', 'Ħ',
-                 'ĳ', 'Œ', 'œ', '☚', '☛', '★', '☆', '♠', '♣', '♥', '♦', '♪', '♫', '♀']
+    scs = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '~', '`', '{', '}', '[', ']', ';', ':',
+           '"', "'", '<', '>', '?', ',', '.', '/', '|']
+    super_s_c = ['←', '↑', '→', '↓', '·', '•', '●', '–', '‽', '‖', '«', '»', '‘', '„', '✅', '❤️', '⌘', '', '⌥', '⌫',
+                 '∞', '™', '¼', '½', '¾', 'À', 'Á', 'Â', 'Ã', 'Ä', 'Å', 'Æ', 'Ç', 'È', 'É', 'Ê', 'Ë', 'Ì', 'Í', 'Î',
+                 'Ï', 'Ð', 'Ñ', 'Ò', 'Ó', 'Ô', 'Õ', 'Ö', 'Ø', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'Þ', 'ß', 'æ', 'Ħ', 'ĳ', 'Œ',
+                 'œ', '☚', '☛', '★', '☆', '♠', '♣', '♥', '♦', '♪', '♫', '♀']
     create = True
     if letter == 'y':
         chars += letters
@@ -34,13 +35,14 @@ def create(letter, num, sc, super_c, length, save, print_info):
         print(password)
 
     if save == 'y':
-        website = input('what webiste will this password be used by?')
-        from Password_saver import add_password, save_passwords
+        website = input('what website will this password be used by?')
+        from password_saver import add_password, save_passwords
         add_password(password, website)
         save_passwords()
         if print_info:
             print('your password, {} has been added to your saved passwords!'.format(password))
     return password
+
 if __name__ == "__main__":
     create(input('Would you like letters? (ex. A, b, c)'),
            input('Would you like numbers? (ex. 1, 2, 3)'),
