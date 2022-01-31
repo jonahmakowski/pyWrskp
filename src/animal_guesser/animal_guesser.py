123814c7f87425b396221b89a5b2be9fd616eac3
jonahmakowski <jonah@makowski.ca>
2022-01-31 16:59:10 -0500

    fixed an issue where the program only works for the bulldog animal

--- src/animal_guesser/animal_guesser.py
+++ src/animal_guesser/animal_guesser.py
@@ -1,7 +1,13 @@
 class AnimalChoser:
     def __init__(self):
-        self.options = [{'name': 'bulldog', 'type': 'dog', 'info': ['fur', 'pet']}]
-        self.atturbites = [{'name': 'fur', 'y/n': ''}, {'name': 'pet', 'y/n': ''}]
+        self.options = [{'name': 'bulldog', 'info': ['fur', 'pet', 'dog', 'short snout']},
+                        {'name': 'human', 'info': ['hair', 'two legs', 'short snout']}]
+        self.atturbites = [{'name': 'fur', 'y/n': ''},
+                           {'name': 'pet', 'y/n': ''},
+                           {'name': 'dog', 'y/n': ''},
+                           {'name': 'short snout', 'y/n': ''},
+                           {'name': 'hair', 'y/n': ''},
+                           {'name': 'two legs', 'y/n': ''}]
         self.correct_atturbites = []
         print('The current options for this game are:')
         for item in self.options:
@@ -17,10 +23,29 @@ class AnimalChoser:
             if item['y/n'] == 'y':
                 self.correct_atturbites.append(item['name'])
 
+        choosen = False
+
+        for item in self.options:
+            item['info'] = sorted(item['info'])
+
+        self.correct_atturbites = sorted(self.correct_atturbites)
+
         for item in self.options:
             if item['info'] == self.correct_atturbites:
                 print('your animal is {}'.format(item['name']))
+                choosen = True
                 break
+
+        if not choosen:
+            print("This program can figure out what you choose, make sure it is on this list:")
+            for item in self.options:
+                print(item['name'])
+
+            print('debug info:')
+            print('self.correct_atturbites:')
+            print(self.correct_atturbites)
+            print('self.options:')
+            print(self.options)
 
 
 game = AnimalChoser()
