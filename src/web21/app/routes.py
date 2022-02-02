f4ec49d657e8bd62a31866d2a26a96ab26a8c0c3
jonahmakowski <jonah@makowski.ca>
2022-02-02 16:28:46 -0500

    removed fuction that did not work
    I tried before to make this work, but I think that the app called tkinter does not like being call from a flask app.

--- src/web21/app/routes.py
+++ src/web21/app/routes.py
@@ -415,24 +415,3 @@ def schedule_empty():
     s.empty()
 
     return redirect('/schedule/create')
-
-
-'''
-# This code is not working, not sure why, this is the same code as in the orginal page.
-@app.route('/txt', methods=['GET', 'POST'])
-def txt():
-    if request.method == 'POST':
-        sys.path.append(pyWrkspLoc + '/src/other/classes')
-        import class_4_pt_1
-        import tkinter as tk
-        df_save_loc = '/home/jonah/Thonny files/TXT_files/' # change to folder name where you want auto saves as def
-        df_name = 'testing' # you can change def save name
-        root = tk.Tk()
-        root.title('Text Editer')
-        root.rowconfigure(0, minsize=800, weight=1)
-        root.columnconfigure(1, minsize=600, weight=1)
-        app = class_4_pt_1.Application(df_name, df_save_loc, master=root)
-        app.mainloop()
-        return redirect('/')
-    return render_template('txt.html', title='text editer')
-'''
