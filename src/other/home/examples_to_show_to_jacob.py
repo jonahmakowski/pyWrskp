de52cb7ffd1ee20d0643467089818a03d85c86d7
Jonah Makowski <jonah@makowski.ca>
2022-02-04 15:38:48 -0500

    This commit adds more examples
    this adds while True, break, .format, +=, and if

--- src/other/home/examples_to_show_to_jacob.py
+++ src/other/home/examples_to_show_to_jacob.py
@@ -27,7 +27,7 @@ var7 = 0.5  # makes a var that is = to the float 0.5
 
 var8 = True # this is a boolean value, True or False (it has to start with a capitol in python)
 
-var9 = [var1, var2, var3, var4, var5, var6, var7]  # This makes a var = to a list of all of the other vars
+var9 = [var1, var2, var3, var4, var5, var6, var7, var8]  # This makes a var = to a list of all of the other vars
 
 for i in range(3):  # means do 3 times "i" is a counter
     print('hi')
