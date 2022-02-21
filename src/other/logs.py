ca91fe5683d4d72f49d0d99432aa64274d344263
Jonah Makowski <jonah@makowski.ca>
2022-02-21 18:31:22 -0500

    oh no, I frogot to save, here it is

--- src/other/logs.py
+++ src/other/logs.py
@@ -9,48 +9,6 @@ except KeyError:
     pyWrksp = os.environ["HOME"] + input('Since you do not have the PYWRSKP env var '
                                          '\nPlease enter the pwd for the pyWrskp repo not including the '
                                          '"home" section')
-'''
-class Log:
-    def __init__(self, pyWrskp):
-        self.log = []
-        self.load()
-        self.loc = pyWrskp
-        self.name = self.loc + '/docs/txt-files/logs.txt'
-        self.mainloop()
-    
-    def load(self):
-        with open(self.name) as json_file:
-            self.log = json.load(json_file)
-    
-    def save(self):
-        with open(self.name, 'w') as outfile:
-            json.dump(self.log, outfile)
-    
-    def mainloop(self):
-        while True:
-            do = input('What do you want to do?')
-            if do == 'add':
-                self.add()
-                break
-            elif do == 'read':
-                self.read()
-                break
-            else:
-                print('This is not an option')
-                print('Try again')
-    
-    def add(self):
-        log_info = input('What do you want to add to the log?\n')
-        now = str(datetime.datetime.now())
-        self.log.append({'time': now, 'info': log_info})
-        self.save()
-    
-    def read(self):
-        self.load()
-        print('Time\t\t\tInfo')
-        for item in self.log:
-            print('{}\t\t\t{}'.format(item['time'], item['info']))
-'''
 
 class Log:
     def __init__(self, pyWrskp):
@@ -92,4 +50,6 @@ class Log:
         print('Time\t\t\tInfo')
         for item in self.log:
             print('{}\t\t\t{}'.format(item['time'], item['info']))
+
+
 log = Log(pyWrksp)
