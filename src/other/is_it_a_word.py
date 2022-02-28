e61b06e4e2bcdaf99900a256954deacda6e53158
Jonah Makowski <jonah@makowski.ca>
2022-02-28 16:06:46 -0500

    finished is_it_a_word

--- src/other/is_it_a_word.py
+++ src/other/is_it_a_word.py
@@ -3,12 +3,20 @@ class IsItAWord:
         self.word = word
         self.word_list = list(word)
         self.rever = ''
+        self.reverse()
 
     def reverse(self):
-        for i in range(len(self.word_list)):
-            self.rever += self.word_list[]
-            print(self.rever)
+        for i in range(len(self.word_list) -1, -1, -1):
+            self.rever += self.word_list[i]
+            #print(self.rever)
+            #print(i)
+    def check(self):
+        if self.rever == self.word:
+            print('If you reverse the word {} you will get the same word'.format(self.word))
+        else:
+            print('If you reverse the word {} you get {}'.format(self.word, self.rever))
+            print('So it does not work')
 
 
 is_it_a_word = IsItAWord('add')
-is_it_a_word.reverse()
+is_it_a_word.check()
