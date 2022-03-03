15476a8e2e8f1d271aa5096517fc4ac8b4176ee6
jonahmakowski <jonah@makowski.ca>
2022-03-03 16:43:56 -0500

    Added a reverse word checker to the website

--- src/other/is_it_the_word.py
+++ src/other/is_it_the_word.py
@@ -6,10 +6,11 @@ class IsItAWord:
         self.reverse()
 
     def reverse(self):
-        for i in range(len(self.word_list) -1, -1, -1):
+        for i in range(len(self.word_list) - 1, -1, -1):
             self.rever += self.word_list[i]
-            #print(self.rever)
-            #print(i)
+            # print(self.rever)
+            # print(i)
+
     def check(self):
         if self.rever == self.word:
             print('If you reverse the word {} you will get the same word'.format(self.word))
@@ -17,6 +18,13 @@ class IsItAWord:
             print('If you reverse the word {} you get {}'.format(self.word, self.rever))
             print('So it does not work')
 
+    def check_blind(self):
+        if self.rever == self.word:
+            return 'If you reverse the word {} you will get the same word'.format(self.word)
+        else:
+            return 'If you reverse the word {} you get {}'.format(self.word, self.rever)
 
-is_it_a_word = IsItAWord(input('What word would you like to check?'))
-is_it_a_word.check()
+
+if __name__ == "__main__":
+    is_it_a_word = IsItAWord(input('What word would you like to check?'))
+    is_it_a_word.check()
