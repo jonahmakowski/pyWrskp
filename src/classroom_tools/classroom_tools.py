895eee2a9cb4fd688ca76218f5cd9b863f2b2fd0
jonahmakowski <jonah@makowski.ca>
2022-03-08 20:24:50 -0500

    Created challange game mode for the math game

---
+++ src/classroom_tools/classroom_tools.py
@@ -0,0 +1,63 @@
+from random import randint as r
+import classroom_tools_extras
+
+
+class Classroom:
+    def __init__(self):
+        self.roster = []
+        self.teacher = ''
+        self.princaple = ''
+        self.school_name = ''
+        self.add_roster()
+
+    def add_roster(self):
+        print('Enter all of the students names:')
+        while True:
+            name = input('')
+            if name == '':
+                break
+            self.roster.append(name)
+
+    def team_maker(self):
+        num_of_teams = input('How many teams would you like?')
+        teams = []
+        for i in range(int(num_of_teams)):
+            teams.append([])
+        roster_copy = self.roster
+        
+        while len(roster_copy) >= int(num_of_teams):
+            for item in teams:
+                person_num = r(0, len(roster_copy) - 1)
+                person_name = roster_copy[person_num]
+                del roster_copy[person_num]
+                item.append(person_name)
+        
+        if len(roster_copy) > 0:
+            i = 0
+            while len(roster_copy) > 0:
+                teams[i].append(roster_copy[0])
+                del roster_copy[0]
+                i += 1
+        
+        team_num = 0
+        for item in range(len(teams)):
+            print('Team {}'.format(team_num + 1))
+            for i in range(len(teams[team_num])):
+                print(teams[team_num][i])
+            team_num += 1
+            print('\n\n')
+
+    def math_game(self):
+        ty = input('What type of game would you like?')
+        if ty == 'normal':
+            classroom_tools_extras.basic_math_game()
+        elif ty == 'challange':
+            classroom_tools_extras.challange_math_game(self.roster)
+
+
+testing_class = Classroom()
+do = input('What do you want to do?')
+if do == 'team maker':
+    testing_class.team_maker()
+elif do == 'math game':
+    testing_class.math_game()
---
+++ src/classroom_tools/classroom_tools_extras.py
@@ -0,0 +1,69 @@
+from random import randint as r
+
+
+def math_game(coins):
+    opration = r(1, 4)
+    num_1 = r(0, 9999999)
+    num_2 = r(0, 9999999)
+    if opration == 1:
+        opration = '+'
+        question = '{} {} {}'.format(num_1, opration, num_2)
+        a = str(num_1 + num_2)
+    elif opration == 2:
+        opration = '-'
+        question = '{} {} {}'.format(num_1, opration, num_2)
+        a = str(num_1 - num_2)
+    elif opration == 3:
+        opration = '*'
+        question = '{} {} {}'.format(num_1, opration, num_2)
+        a = str(num_1 * num_2)
+    elif opration == 4:
+        opration = '/'
+        question = '{} {} {}'.format(num_1, opration, num_2)
+        a = str(num_1 / num_2)
+    else:
+        print('error')
+        question = 'error'  # this is to solve an error found by pycharm
+        a = 'error'  # this is to solve an error found by pycharm
+        exit(404)
+    g = input('What is ' + question + '?')
+    if g == a:
+        print('Good job you got it correct! you earned 10 coins')
+        coins += 10
+        print('You now have {} coins'.format(coins))
+    else:
+        print('You got it incorrect, you lost 5 coins')
+        coins -= 5
+        print('You now have {} coins'.format(coins))
+        if g == '':
+            print('if you are playing basic mode this coins will be returned')
+    return g, coins
+
+def basic_math_game():
+    print('WELCOME TO')
+    print("JONAH'S MATH GAME!")
+    print('entering nothing will end the game!')
+    coins = 0
+    while True:
+        g, coins = math_game(coins)
+        if g == '':
+            coins += 5
+            return
+
+    print('GAME OVER')
+    print('You have {} coins right now'.format(coins))
+
+def challange_math_game(people):
+    scores = {}
+    while len(people) > 0:
+        current_player = people[0]
+        print('Your turn {}'.format(current_player))
+        coins = 0
+        for i in range(4):
+            g, coins = math_game(coins)
+        scores[current_player] = coins
+        del people[0]
+        
+    print('Final scores:')
+    for person in scores:
+        print(str(person) + ': ' + str(scores[person]))
