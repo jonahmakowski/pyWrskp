d6e85668063e83a2b497ed9161954531f6f2f77b
jonahmakowski <jonah@makowski.ca>
2022-03-24 18:29:04 -0400

    Created a message sender
    This message sender works in a way that it creates a .txt file that you can than airdrop or otherwise share with your friends, and if they run this program and enter the passcode then they can read the message.

---
+++ src/super_safe_note_sender/sender.py
@@ -0,0 +1,61 @@
+import sys
+import json
+import random
+import os
+sys.path.append('../coder-decoder')
+from coder import CoderDecoder as CD
+
+
+class Sender:
+    def __init__(self):
+        self.remote_coder = CD(print_info=False)
+        self.remote_coder_2 = CD(print_info=False)
+
+    def create_note(self):
+        print('What would you like the password to be?')
+        password = input('The person who decodes this needs to know it')
+        message = input('What is the message that you would like to send?')
+        key = random.randint(0, len(self.remote_coder.abcs) - 1)
+
+        self.remote_coder.add_vars(message, key)
+        message = self.remote_coder.code()
+
+        self.remote_coder_2.add_vars(password, key)
+        password = self.remote_coder_2.code()
+
+        save_dic = {'password': password, 'message': message, 'key': key}
+        with open('message.txt', 'w') as outfile:
+            json.dump(save_dic, outfile)
+
+    def read_note(self):
+        try:
+            with open('message.txt') as json_file:
+                dic = json.load(json_file)
+        except FileNotFoundError:
+            print('There is no file like this (make sure it is called message.txt)')
+            exit(404)
+
+        password = input('What is the password?')
+
+        self.remote_coder_2.add_vars(dic['password'], dic['key'])
+        password_check = self.remote_coder_2.decode()
+
+        if password != password_check:
+            print('password incorrect deleting file')
+            os.remove("message.txt")
+            exit(500)
+
+        self.remote_coder.add_vars(dic['message'], dic['key'])
+        message = self.remote_coder.decode()
+
+        print('The message in this file is:')
+        print(message)
+
+
+if __name__ == '__main__':
+    sender = Sender()
+    do = input('What do you wish to do?')
+    if do == 'create':
+        sender.create_note()
+    elif do == 'read':
+        sender.read_note()
