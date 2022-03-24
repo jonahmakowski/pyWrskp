c2fc1ad288b9361e06770bf5da908d6043d4415a
jonahmakowski <jonah@makowski.ca>
2022-03-24 19:03:24 -0400

    Changed program to make it harder to decrypt

--- src/super_safe_note_sender/sender.py
+++ src/super_safe_note_sender/sender.py
@@ -10,6 +10,7 @@ class Sender:
     def __init__(self):
         self.remote_coder = CD(print_info=False)
         self.remote_coder_2 = CD(print_info=False)
+        self.remote_coder_3 = CD(print_info=False)
 
     def create_note(self):
         print('What would you like the password to be?')
@@ -23,7 +24,10 @@ class Sender:
         self.remote_coder_2.add_vars(password, key)
         password = self.remote_coder_2.code()
 
-        save_dic = {'password': password, 'message': message, 'key': key}
+        self.remote_coder_3.add_vars(str(key), 15)
+        key = self.remote_coder_3.code()
+
+        save_dic = {'asuydhausdhuashd': password, 'sdifhuegtsydftyas': message, 'asdyatsdftras': key}
         with open('message.txt', 'w') as outfile:
             json.dump(save_dic, outfile)
 
@@ -35,9 +39,12 @@ class Sender:
             print('There is no file like this (make sure it is called message.txt)')
             exit(404)
 
+        self.remote_coder_3.add_vars(dic['asdyatsdftras'], 15)
+        key = int(self.remote_coder_3.decode())
+
         password = input('What is the password?')
 
-        self.remote_coder_2.add_vars(dic['password'], dic['key'])
+        self.remote_coder_2.add_vars(dic['asuydhausdhuashd'], key)
         password_check = self.remote_coder_2.decode()
 
         if password != password_check:
@@ -45,11 +52,13 @@ class Sender:
             os.remove("message.txt")
             exit(500)
 
-        self.remote_coder.add_vars(dic['message'], dic['key'])
+        self.remote_coder.add_vars(dic['sdifhuegtsydftyas'], key)
         message = self.remote_coder.decode()
 
         print('The message in this file is:')
         print(message)
+        print('destroying file')
+        os.remove('message.txt')
 
 
 if __name__ == '__main__':
