64d39c859558e741951494a438416fb4cef67dbe
jonahmakowski <jonah@makowski.ca>
2022-04-17 08:11:59 -0400

    Made a very basic version of the game i have in python

---
+++ src/Java/src/com/Game/Game.java
@@ -0,0 +1,37 @@
+package com.Game;
+import java.util.*;
+
+public class Game {
+    public static void main(String[] args) {
+        //player info
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name= input.nextLine();
+        int health = 100;
+        if (name.equals("Jonah")) {
+            health = health * 2;
+        }
+        System.out.println("You have " + health + " hp.");
+        System.out.println("The attacks are:");
+        System.out.println("super - with 10 damage");
+
+        // monster info
+
+        int monster_health = 100;
+        System.out.println("The monster has " + monster_health + " hp.");
+
+        String attack= "";
+
+        while (monster_health > 0 && health > 0) {
+            System.out.println("What attack do you want to use?");
+            attack = input.nextLine();
+            if (attack.equals("super")) {
+                monster_health = monster_health - 10;
+                System.out.println("You did 10 damage the monster now has " + monster_health + " hp");
+            }
+            health = health - 10;
+            System.out.println("The monster did 10 damage");
+        }
+    }
+}
