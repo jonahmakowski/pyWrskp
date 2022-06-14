7f5042eac4a992ba99a825cbf7896fa6fd3dd49c
jonahmakowski <jonah@makowski.ca>
2022-06-13 21:41:17 -0400

    Made a basic app

---
+++ src/App/FirstApp/FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Jonah Makowski on 2022-06-13.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
