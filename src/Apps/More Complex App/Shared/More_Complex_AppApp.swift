b25fc7f75c7f685598dd2305c3b7f65cedf09e60
jonahmakowski <jonah@makowski.ca>
2022-06-14 20:55:58 -0400

    Mixed error

---
+++ src/Apps/More Complex App/Shared/More_Complex_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  More_Complex_AppApp.swift
+//  Shared
+//
+//  Created by Jonah Makowski on 2022-06-14.
+//
+
+import SwiftUI
+
+@main
+struct More_Complex_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
