4f1ac6b3a6ef25a09ba08fb2158c59c98d6b087f
jonahmakowski <jonah@makowski.ca>
2022-06-26 19:18:55 -0400

    Added a ton of new features to the grid program
    This can plot points, and draw the shapes!

--- src/turtle_math/turtle_grid.py
+++ src/turtle_math/turtle_grid.py
@@ -5,6 +5,7 @@ class Grid:
     def __init__(self):
         self.t = turtle.Turtle()
         self.t.speed(0)
+        self.t.pensize(1)
         self.draw_grid()
         self.found_cors = []
 
@@ -75,27 +76,42 @@ class Grid:
         self.found_cors.append([xcor, ycor])
 
     def draw(self):
+        self.t.pensize(5)
+
         if len(self.found_cors) > 2:
             self.t.penup()
-            xcor = self.found_cors[0][0]
-            ycor = self.found_cors[0][1]
-            self.t.goto(xcor, ycor)
-            self.t.pendown()
-            del self.found_cors[0]
 
             for item in self.found_cors:
                 temp_xcor = item[0]
                 temp_ycor = item[1]
                 self.t.goto(temp_xcor, temp_ycor)
+                self.t.pendown()
 
-            self.t.goto(xcor, ycor)
-            for item in self.found_cors:
-                del item
+            self.t.goto(self.found_cors[0][0], self.found_cors[0][1])
+
+            for i in self.found_cors:
+                del i
+        elif len(self.found_cors) == 1:
+            print('There is only one dot, so this does not work')
+        elif len(self.found_cors) == 2:
+            self.t.penup()
+            self.t.goto(self.found_cors[0][0], self.found_cors[0][1])
+            self.t.pendown()
+            self.t.goto(self.found_cors[1][0], self.found_cors[1][1])
+        self.t.pensize(1)
 
 
 C = Grid()
+
 C.find_cor(4, 10)
 C.find_cor(10, 4)
 C.find_cor(1, 4)
 C.draw()
-input()
+
+C.find_cor(6, 20)
+C.find_cor(20, 6)
+C.find_cor(3, 4)
+C.find_cor(20, 20)
+C.draw()
+
+input('Press enter to finish program')
