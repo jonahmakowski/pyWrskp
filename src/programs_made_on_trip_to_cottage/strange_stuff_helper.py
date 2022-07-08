c69174572780b50c2f5f7ce7f68950077b560698
Jonah Makowski <jonah@makowski.ca>
2022-07-08 14:17:20 -0400

    Made a funny program

---
+++ src/programs_made_on_trip_to_cottage/strange_stuff_helper.py
@@ -0,0 +1,26 @@
+import time
+
+def clear_screen():
+    for i in range(100):
+        print()
+def black_screen():
+    for i in range(20):
+        print('||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||')
+        
+def white_black(length):
+    while length >= 0:
+        clear_screen()
+        time.sleep(length)
+        black_screen()
+        time.sleep(length)
+        if length >= 4:
+            length -= 1
+        if length >= 3:
+            length -= 0.75
+        if length >= 2:
+            length -= 0.5
+        if length >= 1:
+            length -= 0.25
+        if length < 1:
+            length -= 0.1
+    
