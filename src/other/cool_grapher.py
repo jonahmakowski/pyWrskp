512eee498225af192ad3990ffc9d95bd6829d376
jonahmakowski <jonah@makowski.ca>
2022-12-08 21:33:11 -0500

    Made it a little bit more useful
    This change will help you if you are looking for a really high term (ie: you want term 100000000000000000000000) so you don't have to wait for the list to get to that term.

--- src/other/cool_grapher.py
+++ src/other/cool_grapher.py
@@ -1,24 +1,27 @@
 m = int(input('What is the rate of change?'))
 b = int(input('What is number zero?'))
-x = 1
-threshold = int(input('What x are you looking for?')) + 1
-while True:
-    while x < threshold:
-        print()
-        print()
-        number = m * x
-        number += b
-        print(number)
-        print(x)
-        if threshold > 1000000:
-            if threshold > 1000000000000:
-                x += 100000000000
-            else:
-                x += 10
-        else:
+
+mode = input('Would you like a list or one answer? (1/2)')
+
+if mode == '1':
+    x = 1
+    threshold = 100000000000000000000000000000000000000000000
+    while True:
+        while x < threshold:
+            print()
+            print()
+            number = m * x
+            number += b
+            print(number)
+            print(x)
             x += 1
-    cont = input('x = {}, would you like to continue? y/n\nif y, x will increase by 100'.format(x - 1))
-    if cont == 'n':
-        break
-    else:
+        cont = input('would you like to continue? y/n')
+        if cont == 'n':
+            break
         threshold += 100
+
+elif mode == '2':
+    x = int(input('What term are you looking for?'))
+    number = m * x
+    number += b
+    print('That is {}!'.format(number))
