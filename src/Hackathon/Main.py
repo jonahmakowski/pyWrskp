e5de732117120eafdcf6682d4657a84f6fd8402c
Jonah Makowski <jonah@makowski.ca>
2022-12-10 10:46:16 -0500

    made a flying rectangle!

--- src/Hackathon/Main.py
+++ src/Hackathon/Main.py
@@ -8,10 +8,26 @@ class Dino:
     def __init__(self):
         self.RED = (255, 0, 0)
         self.BLUE = (0, 0, 255)
+        self.BLACK = (0, 0, 0)
         self.display = pygame.display.set_mode((400, 300))
         
-        pygame.draw.rect(self.display, self.BLUE, pygame.Rect(90, 30, 60, 120))
-        pygame.draw.rect(self.display, self.RED, pygame.Rect(30, 30, 60, 120))
+        pygame.draw.rect(self.display, self.BLUE, pygame.Rect(90, 175, 60, 120))
+        pygame.draw.rect(self.display, self.RED, pygame.Rect(30, 175, 60, 120))
         pygame.display.flip()
+        
+        time.sleep(1)
+        
+        self.jump()
+        
+    def jump(self):
+        y = 175
+        while True:
+            self.display.fill(self.BLACK)
+            
+            pygame.draw.rect(self.display, self.BLUE, pygame.Rect(90, y, 60, 120))
+            pygame.draw.rect(self.display, self.RED, pygame.Rect(30, y, 60, 120))
+            pygame.display.flip()
+            
+            y -= 1
 
 Test = Dino()
