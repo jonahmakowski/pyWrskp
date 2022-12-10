6b78e13c6c5f4d365acbc1e78f3042286d3c936b
kneecapassassin <120265155+kneecapassassin@users.noreply.github.com>
2022-12-10 13:56:55 -0500

    Update sarim.py

--- src/Hackathon/sarim.py
+++ src/Hackathon/sarim.py
@@ -1,8 +1,36 @@
 import pygame
 import time
 import random
+import os
+import sys
+import math
+
 
 pygame.init()
+
+bg = pygame.image.load(os.path.join('images','bg.png')).convert()
+bgX = 0
+bgX2 = bg.get_width()
+
+run = True
+speed = 30  # NEW
+
+while run:
+    clock.tick(speed)  # NEW
+    bgX -= 1.4  # Move both background images back
+    bgX2 -= 1.4
+
+    if bgX < bg.get_width() * -1:  # If our bg is at the -width then reset its position
+        bgX = bg.get_width()
+    
+    if bgX2 < bg.get_width() * -1:
+        bgX2 = bg.get_width()
+
+    for event in pygame.event.get():  
+        if event.type == pygame.QUIT: 
+            run = False    
+            pygame.quit() 
+            quit()
 
 width = 650
 height = 750
