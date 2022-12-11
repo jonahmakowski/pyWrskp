d9bb93a64168b191a8692ea4a63c28e450700a85
Jonah Makowski <jonah@makowski.ca>
2022-12-10 19:58:12 -0500

    final version of our hackathon assignment

--- src/Hackathon/sarim.py
+++ src/Hackathon/sarim.py
@@ -9,34 +9,54 @@ import os
 pygame.init()
 
 
-x = 50
-y = 50
-width = 40
-height = 60
-vel = 5
 
-run = True
+class Dino:
+    def __init__(self):
+        self.RED = (255,0,0)
+        self.BLUE = (0, 0, 255)
+        self.BLACK = (0, 0, 0)
+        self.display = pygame.display.set_mode((400, 300))
+        
+        pygame.draw.rect(self.display, self.BLUE, pygame.Rect(90, 180, 60, 120))
+        pygame.draw.rect(self.display, self.RED, pygame.Rect(30, 180, 60, 120))
+        pygame.display.flip()
+        
+   
+        
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        self.jump()
+                
+        
+    def jump(self):
+        y = 180
+        while True:
+            self.display.fill(self.BLACK)
+            
+            pygame.draw.rect(self.display, self.BLUE, pygame.Rect(90, y, 60, 120))
+            pygame.draw.rect(self.display, self.RED, pygame.Rect(30, y, 60, 120))
+            pygame.display.flip()
+            
+            y -= 1.5
+            if y < 100:
+                break
+        
+        while True:
+            self.display.fill(self.BLACK)
+            
+            pygame.draw.rect(self.display, self.BLUE, pygame.Rect(90, y, 60, 120))
+            pygame.draw.rect(self.display, self.RED, pygame.Rect(30, y, 60, 120))
+            pygame.display.flip()
+            
+            y += 2
+            time.sleep(0.001)
+            if y == 175:
+                break
+            
+        
+        
 
-while run:
-    pygame.time.delay(100)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-
-    keys = pygame.key.get_pressed()
-    
-    if keys[pygame.K_LEFT]:
-        x -= vel
-
-    if keys[pygame.K_RIGHT]:
-        x += vel
-
-    if keys[pygame.K_UP]:
-        y -= vel
-
-    if keys[pygame.K_DOWN]:
-        y += vel
-    
-
+Test = Dino()
 
