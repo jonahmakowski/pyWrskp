2e8bc6a7ec0c72e0668c313da5d51ae9b9981e1b
jonahmakowski <jonah@makowski.ca>
2022-12-22 17:36:46 -0500

    Made an annoying program
    All this is supposed to do is be annoying when you enter beeped.

---
+++ src/music/annoying.py
@@ -0,0 +1,11 @@
+import pygame
+import time
+
+pygame.init()
+
+while True:
+    input('press enter to continue')
+
+    pygame.mixer.music.load('../music/alarm.mp3')
+    pygame.mixer.music.play()
+    time.sleep(10)
