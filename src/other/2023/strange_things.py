65e0f91c2f4a4c1a8288b96d12add2f8ff674535
jonahmakowski <jonah@makowski.ca>
2023-01-03 13:50:23 -0500

    I made an interesting program

---
+++ src/other/2023/strange_things.py
@@ -0,0 +1,22 @@
+import turtle
+
+t = turtle.Turtle()
+
+max = 700
+
+length = max
+
+t.speed(0)
+
+t.penup()
+t.goto(-400, 400)
+t.pendown()
+
+while True:
+    t.forward(length)
+    t.right(90)
+    length -= 4
+    if length < max * -1:
+        break
+
+input()
