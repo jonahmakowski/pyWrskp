5f9733f9a79ebd396cebfd7cfa19ea0a7ed289e8
jonahmakowski <jonah@makowski.ca>
2023-02-06 19:56:07 -0500

    Made the website a little more interesting

--- src/new_web/app/routes.py
+++ src/new_web/app/routes.py
@@ -1,9 +1,31 @@
 from flask import render_template
 from app import app
 
+pages = [{'name': 'home', 'locations': ['/', '/home', '/main'], 'html': 'home.html'},
+         {'name': 'new site info', 'locations': ['/new_site_info'], 'html': 'new_site.html'},
+         {'name': 'print_web_info', 'locations': ['/print_web_info'], 'html': 'info.html'}]
+
 
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/home', methods=['POST', 'GET'])
 @app.route('/main', methods=['POST', 'GET'])
 def home():
     return render_template('home.html', title='Welcome')
+
+
+@app.route('/new_site_info', methods=['POST', 'GET'])
+def new_site():
+    return render_template('new_site.html', title='New Site Info')
+
+
+@app.route('/print_web_info')
+def print_web_info():
+    for item in pages:
+        print('Name: {}'.format(item['name']))
+        print('Locations:')
+        for t in item['locations']:
+            print('\t{}'.format(t))
+        print('HTML: {}'.format(item['html']))
+        print()
+        print()
+    return render_template('info.html', title='Pages Info')
