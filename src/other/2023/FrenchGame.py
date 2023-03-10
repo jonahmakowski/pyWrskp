4f902d70220921f00969fb9def3b02d1a1040352
Jonah Makowski <jonah@makowski.ca>
2023-03-10 11:26:25 -0500

    Upgraded the game
    I need to do some more updates at home to make it more interesting

--- src/other/2023/FrenchGame.py
+++ src/other/2023/FrenchGame.py
@@ -1,4 +1,5 @@
 from random import randint
+import time
 
 
 class FrenchAssignment:
@@ -8,25 +9,43 @@ class FrenchAssignment:
     def trivia(self):
         questions = [{'question': 'Qui a chanté Sur Va Yeke?', 'awnser': 'Black M'},
                      {'question': 'Qui a chanté Outété?', 'awnser': 'KeenV'},
-                     {'question': 'combien de temps dure Outété? (en une minute:seconde)', 'awnser': '4:29'}]
+                     {'question': 'Combien de temps dure Outété? (en format minute:seconde)', 'awnser': '4:29'},
+                     {'question': 'Combien de temps dure Rifia? (en format minute:seconde)', 'awnser': '3:02'},
+                     {'question': 'Qui a gagné le premier tour?', 'awnser': 'Outete'},
+                     {'question': 'Qui a gagné la deuxième ronde?', 'awnser': 'Danser avec toi'},
+                     {'question': 'Qui a gagné la troisième ronde?', 'awnser': 'Case Depart'},
+                     {'question': 'Qui a gagné la quatrième ronde?', 'awnser': 'Demain ca ira'}]
         print('BIENVENUE AU JEU-QUESTIONNAIRE!')
+        time.sleep(1)
+        print('Quand il y a une question comme celle-ci: Combien de temps dure ____, le temps est pris de la vidéo youtube.')
+        time.sleep(1)
+        print('Tous les awnsers sont sans accent.')
+        time.sleep(1)
+        print('Bienvenue {}!'.format(self.user))
+        print('Le jeu commencera dans deux secondes.')
+        time.sleep(2)
         while len(questions) > 1:
             q, questions = self.choose_random(questions, 1)
+            if not q:
+                break
             print(q[0]['question'])
             awn = input('')
             awn = awn.lower()
             if awn == q[0]['awnser'].lower():
-                print('Good Job, You are correct')
+                print('Bon travail, vous avez raison!')
                 self.score += 1
-                print('Your current score is {}'.format(self.score))
+                print('Votre note actuelle est {}'.format(self.score))
             else:
                 print('incorrect!')
-                print('Your current score is {}'.format(self.score))
+                print('Votre note actuelle est {}'.format(self.score))
+                print('Le bon awnser est {}'.format(q[0]['question']))
+        print('Le jeu-questionnaire est terminé!')
+        print('Vous avez {} points!'.format(self.score))
     
     def choose_random(self, l, amount):
         if len(l) < amount:
             print('choose_random has encountered an error')
-            return False
+            return False, False
         li = l
         r = []
         for i in range(amount):
@@ -34,6 +53,8 @@ class FrenchAssignment:
             r.append(li[index])
             del li[index]
         return r, li
+    
+    def 
 
 
 assign = FrenchAssignment(input('Nom d’utilisateur:\n'))
