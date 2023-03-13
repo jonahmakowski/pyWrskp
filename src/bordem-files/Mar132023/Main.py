3ca2edc9e16a799e7e6dc4df2ccf2fc368c0d10a
Jonah Makowski <jonah@makowski.ca>
2023-03-13 09:05:11 -0400

    Made a new program
    So far all it does is a security system, will imporve apon it later.

---
+++ src/bordem-files/Mar132023/Main.py
@@ -0,0 +1,27 @@
+import time
+import random
+import json
+from helper import *
+#random.choice(lis)
+
+class Main:
+    def __init__(self):
+        self.login_check = False
+        self.security_clearence = 0
+        self.username = ''
+        self.name = ''
+        self.login()
+    def login(self):
+        info = decrypt_txt()
+        user = input('input your username\n')
+        password = input('input you password\n')
+        for item in info:
+            if item['user'] == user and item['password'] == password:
+                print('successfully logged in!')
+                self.login_check = True
+                self.security_clearence = item['clearance']
+                self.username = item['user']
+        if not self.login_check:
+            exit()
+
+m = Main()
---
+++ src/bordem-files/Mar132023/helper.py
@@ -0,0 +1,77 @@
+import json
+
+
+
+class Encrption:
+    def __init__(self):
+        self.abcs = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+                     'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                     'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+                     'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+                     'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', '.', ',',
+                     '"', "'", '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                     '0', '-', '_', '+', '=', '{', '}', '[', ']', '(', ')',
+                     '!', '?', '|', '<', '>', '/']
+
+    def encrypt(self, item, key):
+        item_list = list(item)
+        completed_list = []
+        for i in item_list:
+            added = False
+            for t in range(len(self.abcs)):
+                if i == self.abcs[t]:
+                    t_key = t + key
+                    if t_key <= len(self.abcs) -1:
+                        completed_list.append(self.abcs[t_key])
+                    else:
+                        t_key_len = len(self.abcs) - t_key
+                        t_key_len = t_key_len * -1
+                        completed_list.append(self.abcs[t_key_len])
+                    added = True
+            if not added:
+                completed_list.append(i)
+        completed_string = ''
+        for item in completed_list:
+            completed_string += item
+        return completed_string
+    def decrypt(self, item, key):
+        item_list = list(item)
+        completed_list = []
+        key = key * -1
+        for i in item_list:
+            added = False
+            for t in range(len(self.abcs)):
+                if i == self.abcs[t]:
+                    t_key = t + key
+                    if t_key <= len(self.abcs) -1:
+                        completed_list.append(self.abcs[t_key])
+                    else:
+                        t_key_len = len(self.abcs) - t_key
+                        t_key_len = t_key_len * -1
+                        completed_list.append(self.abcs[t_key_len])
+                    added = True
+            if not added:
+                completed_list.append(i)
+        completed_string = ''
+        for item in completed_list:
+            completed_string += item
+        return completed_string
+
+def decrypt_txt(path='txt.txt'):
+    e = Encrption()
+    end = []
+    with open(path) as json_file:
+        list_encrypt = json.load(json_file)
+    
+    for item in list_encrypt:
+        count_dic = 0
+        key = item['key']
+        count = 0
+        end.append({})
+        for i in item:
+            if i != 'key':
+                i_key = e.decrypt(i, key)
+                i_decrypt = e.decrypt(item[i], key)
+                end[0][i_key] = i_decrypt
+            count += 1
+    return end
