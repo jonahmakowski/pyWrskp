2eda70ea59efc941fdd0bd6455e867a3f0828135
jonahmakowski <jonah@makowski.ca>
2023-05-02 21:30:29 -0400

    Almost finished this

--- src/other/2023/Google Logo.py
+++ src/other/2023/Google Logo.py
@@ -1,59 +1,92 @@
 import turtle
 
-t = turtle.Turtle()
-t.shape('turtle')
-size = 3
-t.shapesize(size, size, size)
-t.penup()
-current_x = -200
-diffrence = 125
-t.goto(current_x, 200)
-current_x += diffrence
-#t.speed(0) # makes turtle go faster remove later
 
-def drawG():
-    global t
-    t.setheading(180)
-    t.pendown()
-    for i in range(270):
-        t.forward(1)
-        t.left(1)
-    t.left(90)
-    t.forward(60)
-def drawO():
-    global t
-    t.setheading(180)
-    t.pendown()
-    for i in range(400):
-        t.forward(1)
-        t.left(1)
+class Google:
+    def __init__(self):
+        self.t = turtle.Turtle()
+        self.t.shape('turtle')
+        self.size = 2
+        self.t.penup()
+        self.current_x = -200 * self.size
+        self.current_y = 200 * self.size
+        if self.current_y > 500:
+            self.current_y = 500
+        if self.current_x > 800:
+            self.current_x = 800
+        self.diffrence = 125 * self.size
+        self.t.speed(0)  # makes turtle go faster remove later
 
-def drawL():
-    global t
-    t.setheading(270)
-    t.pendown()
-    t.forward(100)
-    t.left(90)
-    t.forward(50)
+    def drawg(self):
+        self.t.setheading(180)
+        self.t.pendown()
+        for i in range(270):
+            self.t.forward(1*self.size)
+            self.t.left(1)
+        self.t.left(90)
+        self.t.forward(60*self.size)
+        self.t.penup()
 
-drawG()
+    def drawo(self):
+        self.t.setheading(180)
+        self.t.pendown()
+        for i in range(400):
+            self.t.forward(1*self.size)
+            self.t.left(1)
+        self.t.penup()
 
-t.penup()
-t.goto(current_x, 200)
-current_x += diffrence
-drawO()
+    def drawl(self):
+        self.t.pendown()
+        self.t.setheading(270)
+        self.t.pendown()
+        self.t.forward(115*self.size)
+        self.t.left(90)
+        self.t.forward(75*self.size)
+        self.t.penup()
 
-t.penup()
-t.goto(current_x, 200)
-current_x += diffrence
-drawO()
+    def drawe(self):
+        self.t.pendown()
+        main_distance = 115*self.size
+        self.t.setheading(270)
+        self.t.pendown()
+        self.t.forward(main_distance)
+        self.t.left(90)
+        self.t.forward(75*self.size)
+        self.t.backward(75*self.size)
+        self.t.right(90)
+        self.t.backward(main_distance/2)
+        self.t.left(90)
+        self.t.forward(50*self.size)
+        self.t.backward(50*self.size)
+        self.t.right(90)
+        self.t.backward(main_distance/2)
+        self.t.left(90)
+        self.t.forward(75*self.size)
+        self.t.penup()
 
-t.penup()
-t.goto(current_x, 200)
-current_x += diffrence
-drawG()
+    def drawgoogle(self):
+        self.t.goto(self.current_x, self.current_y)
+        self.current_x += self.diffrence
+        self.drawg()
 
-t.penup()
-t.goto(current_x, 200)
-current_x += diffrence
-drawL()
+        for i in range(2):
+            self.t.goto(self.current_x, self.current_y)
+            self.current_x += self.diffrence
+            self.drawo()
+
+        self.t.goto(self.current_x, self.current_y)
+        self.current_x += self.diffrence / 2
+        self.drawg()
+
+        self.t.goto(self.current_x, self.current_y)
+        self.current_x += self.diffrence
+        self.drawl()
+
+        self.t.goto(self.current_x, self.current_y)
+        self.current_x += self.diffrence
+        self.drawe()
+
+        input()
+
+
+g = Google()
+g.drawgoogle()
