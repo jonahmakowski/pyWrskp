5fca6dcf833be8c4e8afa4b9df0ebb84ff2892c9
Jonah Makowski <jonah@makowski.ca>
2023-05-08 08:49:16 -0400

    Created a new calculator program
    Will add more in the future

---
+++ src/other/2023/strange_math_assignments.py
@@ -0,0 +1,52 @@
+from pyWrskp import *
+from random import randint
+from time import sleep
+import math
+
+
+class StrangeMathAssignemnts:
+    def __init__(self, user):
+        self.user = user
+    def calculator(self):
+        equation = input('Input your equation:\n')
+        try:
+            if '+' in equation:
+                equation_lis = equation.split('+')
+                equation_lis[0] = float(equation_lis[0])
+                equation_lis[1] = float(equation_lis[1])
+                awnser = equation_lis[0] + equation_lis[1]
+            elif '-' in equation:
+                equation_lis = equation.split('-')
+                equation_lis[0] = float(equation_lis[0])
+                equation_lis[1] = float(equation_lis[1])
+                awnser = equation_lis[0] - equation_lis[1]
+            elif '*' in equation:
+                equation_lis = equation.split('*')
+                equation_lis[0] = float(equation_lis[0])
+                equation_lis[1] = float(equation_lis[1])
+                awnser = equation_lis[0] * equation_lis[1]
+            elif '/' in equation:
+                equation_lis = equation.split('/')
+                equation_lis[0] = float(equation_lis[0])
+                equation_lis[1] = float(equation_lis[1])
+                awnser = equation_lis[0] / equation_lis[1]
+            elif '^' in equation:
+                equation_lis = equation.split('^')
+                equation_lis[0] = float(equation_lis[0])
+                equation_lis[1] = float(equation_lis[1])
+                awnser = equation_lis[0] ** equation_lis[1]
+            elif '$' in equation:
+                equation_lis = equation.split('$')
+                equation_lis[0] = float(equation_lis[0])
+                awnser = math.sqrt(equation_lis[0])
+            else:
+                print('{} is not a valid equation.'.format(equation))
+                return
+            print('The anwser is:')
+            print(awnser)
+        except:
+            print('')
+
+
+s = StrangeMathAssignemnts('Jonah')
+s.calculator()
