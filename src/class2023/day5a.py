b73152fd445dec8e364ba82468424191d0e35ba1
jonahmakowski <jonah@makowski.ca>
2023-05-25 14:58:48 -0400

    Did homework

--- src/class2023/day5a.py
+++ src/class2023/day5a.py
@@ -23,22 +23,38 @@ print(people)
 
 print('Creative edition')
 input('press enter to countinue')
-
-t = turtle.Turtle()
 commands = []
-distance = []
+distances = []
 
 while True:
     while True:
         command = input('What command would you like to use? (forward, backward, left, or right)\n')
+        command = command.lower()
         if (command == 'forward' or command == 'backward') or (command == 'left' or command == 'right'):
+            commands.append(command)
+            break
+    while True:
+        distance = input('What number would you like to use, for example 90 degrees right, or 100 units forward!\n')
+        if distance.isdigit():
+            distances.append(int(distance))
+            break
+    continue_loop = input('Press enter to continue, type n to stop')
+    if continue_loop == 'n':
+        break
+    print('\n\n\n')
 
-for i in range(0, len(commands)-1):
+t = turtle.Turtle()
+
+for i in range(0, len(commands)):
+    # print('Command {}'.format(commands[i]))
+    # print('Distance {}'.format(distances[i]))
     if commands[i] == 'forward':
-        t.forward(distance[i])
+        t.forward(distances[i])
     elif commands[i] == 'right':
-        t.right(distance[i])
+        t.right(distances[i])
     elif commands[i] == 'backward':
-        t.backward(distance[i])
+        t.backward(distances[i])
     elif commands[i] == 'left':
-        t.left(distance[i])
+        t.left(distances[i])
+
+input('Press enter to end program')
