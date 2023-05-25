746ec9c6ae6a92d82548df9638608d175d8154da
jonahmakowski <jonah@makowski.ca>
2023-05-25 07:57:45 -0400

    Started expanded homework

--- src/class2023/day5a.py
+++ src/class2023/day5a.py
@@ -1,9 +1,10 @@
 from random import shuffle
+import turtle
 
 print('Normal edition')
 input('press enter to countinue')
 
-people = ['Brad', 'Dylan', 'Elias', 'Jonah', 'Mahathi', 'Michael', 'Nathan', 'Yonas']
+people = ['Brad', 'Dylan', 'Elias', 'Jonah', 'Mahathi', 'Michael', 'Nathan', 'Yonas', 'John']
 shuffle(people)
 print('Randomly sorted')
 print(people)
@@ -23,3 +24,21 @@ print(people)
 print('Creative edition')
 input('press enter to countinue')
 
+t = turtle.Turtle()
+commands = []
+distance = []
+
+while True:
+    while True:
+        command = input('What command would you like to use? (forward, backward, left, or right)\n')
+        if (command == 'forward' or command == 'backward') or (command == 'left' or command == 'right'):
+
+for i in range(0, len(commands)-1):
+    if commands[i] == 'forward':
+        t.forward(distance[i])
+    elif commands[i] == 'right':
+        t.right(distance[i])
+    elif commands[i] == 'backward':
+        t.backward(distance[i])
+    elif commands[i] == 'left':
+        t.left(distance[i])
