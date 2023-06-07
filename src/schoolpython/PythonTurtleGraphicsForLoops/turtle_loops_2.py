92c2959714edc685d17373db88f4f361c1729ff9
Jonah Makowski <jonah-accounts@makowski.at>
2023-06-07 12:19:25 -0400

    Working on school stuff

--- src/schoolpython/PythonTurtleGraphicsForLoops/turtle_loops_2.py
+++ src/schoolpython/PythonTurtleGraphicsForLoops/turtle_loops_2.py
@@ -1,8 +1,8 @@
 import turtle
 
 def display():
-    for x in range(100, 104):
+    for x in range(50, 50):
         turtle.forward(x)
         turtle.left(90)
-        
+
 display()
---
+++ src/schoolpython/PythonTurtleGraphicsForLoops/turtle_loops_3.py
@@ -0,0 +1,7 @@
+import turtle
+
+def display():
+    for x in range(100, 110):
+        turtle.circle(x)
+
+display()
