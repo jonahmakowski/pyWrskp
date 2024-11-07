808f51a3af69b0d073923d844c964448f9108891
jonahmakowski <90581140+jonahmakowski@users.noreply.github.com>
2024-11-07 15:37:30 -0500

    Added client updates for messaging.

--- src/noahMessagingSystem/helper.py
+++ src/noahMessagingSystem/helper.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+import json
 
 def in_users(users, user, password=None):
     for u in users:
@@ -25,3 +26,18 @@ def load_users():
             exit()
 
     return users
+
+def user_pages(users):
+    sidebar_dic = {}
+    for user in users:
+        sidebar_dic[user['user']] = '/chat/{}'.format(user['user'].lower())
+
+    return sidebar_dic
+
+def load_chats_from_file():
+    with open('chats.txt', 'r') as f:
+        return json.load(f)
+
+def save_chats_to_file(chats):
+    with open('chats.txt', 'w') as f:
+        f.write(chats)
--- src/noahMessagingSystem/main.py
+++ src/noahMessagingSystem/main.py
@@ -1,20 +1,44 @@
 from flask import Flask
-from flask import render_template, redirect, url_for, session, request, send_from_directory
+from flask import render_template, redirect, url_for, session, request, Response
+from flask_sse import sse
 from helper import *
 
-# Run "gunicorn --config gunicorn.conf.py main:app" for production server
+# Run "gunicorn --config gunicorn.conf.py main:app" for production server (from within this folder)
 
 users = load_users()
 
+'''
+Chats format:
+chats = [{'usernames': [username1, username2], 'chat':[{'username': 'user', 'message': 'Message'}]}]
+'''
+
 app = Flask(__name__)
 app.secret_key = 'your-secret-key'
+app.config["REDIS_URL"] = "redis://localhost:6379/0"
+app.register_blueprint(sse, url_prefix='/stream')
 
-@app.route('/')
+def send_message(message, username, other_user):
+    data = {'message': message,
+            'user_sent': other_user.lower()}
+    sse.publish(data, type='personal', channel=username)
+
+@app.route('/chat')
 def home():
     if 'username' not in session:
         return redirect(url_for('login'))
-    return render_template('home.html', username=session['username'])
+    return render_template('home.html', username=session['username'], sidebar_options=user_pages(users))
 
+@app.route('/chat/<user>')
+def chat(user):
+    return render_template('home.html', username=session['username'], sidebar_options=user_pages(users), chat=user.lower())
+
+@app.route('/')
+def redirect_from_main():
+    return redirect(url_for('home'))
+
+@app.route('/stream')
+def stream():
+    return Response(sse.stream(), content_type='text/event-stream')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -34,6 +58,11 @@ def login():
 def logout():
     session.pop('username', None)
     return redirect(url_for('login'))
+
+@app.route('/trigger')
+def trigger():
+    send_message('Test', 'Jonah', 'Jonah')
+    return 'Attempted Send'
 
 if __name__ == "__main__":
     app.run()
