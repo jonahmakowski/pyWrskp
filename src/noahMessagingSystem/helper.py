b912aa0697f45e921f1f8ce0e72177cad9ffd30f
jonahmakowski <90581140+jonahmakowski@users.noreply.github.com>
2024-11-06 22:31:24 -0500

    Made users load passwords from a .env file

--- src/noahMessagingSystem/helper.py
+++ src/noahMessagingSystem/helper.py
@@ -1,5 +1,27 @@
+from dotenv import load_dotenv
+import os
+
 def in_users(users, user, password):
     for u in users:
         if u['user'] == user and u['password'] == password:
             return True
     return False
+
+def load_users():
+    load_dotenv()
+    with open('usernames.txt', 'r') as f:
+        usernames = f.readlines()
+
+    users = []
+
+    for username in usernames:
+        password = os.getenv(username)
+        if password is not None:
+            users.append({'user': username, 'password': os.getenv(username)})
+        else:
+            print('User {} does not have a defined password. Terminating'.format(username))
+            exit()
+
+    return users
+
+print(load_users())
