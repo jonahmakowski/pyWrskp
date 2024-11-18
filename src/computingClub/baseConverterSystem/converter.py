1714b2f0080ead0a3cba7c400031123ef7dc5646
jonahmakowski <90581140+jonahmakowski@users.noreply.github.com>
2024-11-18 11:59:07 -0500

    Computing club

---
+++ src/computingClub/baseConverterSystem/converter.py
@@ -0,0 +1,30 @@
+abcs = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+        'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+        'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '!', '@', '#', '$', '%', '^', '&', '*',
+        '(', ')', '-', '_', '+', '=', '.', ',', '[', ']', '{', '}', ':', ';', "'", '"', '?']
+
+def float_to_base_n(flo:int, base:int):
+    result_lis = []
+    while flo != 0:
+        result_lis.append(flo % base)
+        flo = int(flo / base)
+
+    result_str = ''
+
+    result_lis.reverse()
+
+    for num in result_lis:
+        if num <= 9:
+            result_str += str(num)
+        else:
+            if num-10 > len(abcs)-10:
+                print('The base value of {} is too large'.format(base))
+                return
+            else:
+                result_str += abcs[num-10]
+
+
+    return result_str
+
+if __name__ == '__main__':
+    print(float_to_base_n(int(input('Input the number: ')), int(input('Input the base: '))))
