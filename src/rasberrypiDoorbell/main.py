85bbfe128088176a054c17c5e0fb7f117df91824
jonahmakowski <90581140+jonahmakowski@users.noreply.github.com>
2024-11-22 22:44:35 -0500

    added ip config

--- src/rasberrypiDoorbell/main.py
+++ src/rasberrypiDoorbell/main.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-    print(subprocess.run(['play', 'doorbell.wav']))
+    subprocess.run(['play', 'doorbell.wav'])
     return '<h1>Rang Doorbell!</h1>'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
