f4aaddb082e649cf4a211db02c63f9d6f653804d
jonahmakowski <90581140+jonahmakowski@users.noreply.github.com>
2024-12-03 15:11:49 -0500

    Fixed an error in the word chooser

--- src/wordleSolver/five_letters.py
+++ src/wordleSolver/five_letters.py
@@ -1,4 +1,7 @@
 def get_five_letters():
+    invalid_chars = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '_', '~', '.', '$', '[', ']', '{', '}', '%', '!', ',', '/', '(', ')', '\\', '#', '*']
+
+
     five_letters = []
     with open('words.txt', 'r') as file:
         words_temp = file.readlines()
@@ -6,15 +9,27 @@ def get_five_letters():
     words = []
     for word in words_temp:
         word = word.strip()
+        word = word.lower()
         these_words = word.split()
         words.extend(these_words)
 
-    count = 0
     for word in words:
+        word = word.lower()
         if len(word) == 5:
-            count += 1
-            five_letters.append(word.lower())
-            print("{} {}".format(count, word))
+            valid = True
+
+            for letter in word:
+                if letter in invalid_chars:
+                    valid = False
+            
+            if valid:
+                five_letters.append(word.lower())
+                print('Found {}'.format(word.lower()))
+
+    five_letters = list(set(five_letters))
+    five_letters.sort()
+
+    print('Found {} words'.format(len(words)))
 
     with open('five_letter_words.txt', 'w') as file:
         file.write('\n'.join(five_letters) + '\n')
