9191f7645730023209071aafb35025dbb628af6f
jonahmakowski <90581140+jonahmakowski@users.noreply.github.com>
2024-12-05 10:55:12 -0500

    Worked on the math project

--- src/schoolpython/grade9/Cycle4MiniprojectPresentation/main.py
+++ src/schoolpython/grade9/Cycle4MiniprojectPresentation/main.py
@@ -3,20 +3,37 @@ from flask import render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-@app.route('/converter', methods=['GET', 'POST'])
+@app.route('/converter')
 def converter_page():
-    if request.method == 'GET':
-        return render_template('converter.html')
+    return render_template('converter.html')
 
-@app.route('/converter/base_convert', methods=['POST']) 
+@app.route('/converter/base_convert', methods=['POST', 'GET']) 
 def base_convert():
-    base10 = int(request.form['base10'])
-    return render_template('converter.html', base10_result="{:x}".format(base10).upper())
+    if request.method == 'GET':
+        return redirect(url_for('converter_page'))
+    else:
+        base10 = int(request.form['base10'])
+        return render_template('converter.html', base10_result="{:x}".format(base10).upper(), base_base=base10)
 
-@app.route('/converter/hexa_convert', methods=['POST'])
+@app.route('/converter/hexa_convert', methods=['POST', 'GET'])
 def hexa_convert():
-    hexadecimal = request.form['hexadecimal']
-    return render_template('converter.html', hexadecimal_result=str(int(hexadecimal, 16)))
+    if request.method == 'GET':
+        return redirect(url_for('converter_page'))
+    else:
+        hexadecimal = request.form['hexadecimal']
+        return render_template('converter.html', hexadecimal_result=str(int(hexadecimal, 16)), base_hex=hexadecimal)
+
+@app.route('/')
+def redirect_home():
+    return redirect(url_for('home'))
+
+@app.route('/information')
+def home():
+    return render_template('home.html')
+
+@app.route('/bibliography')
+def bibliography():
+    return render_template('bibliography.html')
 
 if __name__ == '__main__':
     app.run()
