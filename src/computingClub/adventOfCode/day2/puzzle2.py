162cb112f60d451165844bfcbb0016412dcc3cdf
jonahmakowski <jonah@makowski.ca>
2025-02-18 11:56:37 -0500

    Did the day 2 code of Advent Challenges

--- src/computingClub/adventOfCode/day2/puzzle2.py
+++ src/computingClub/adventOfCode/day2/puzzle2.py
@@ -1,3 +1,48 @@
+def report_check(reports):
+    safe = 0
+    nots = []
+    for ind, report in enumerate(reports):
+        if is_safe(report):
+            safe += 1
+        else:
+            nots.append(ind)
+    return safe, nots
+
+def is_safe(report):
+    if check_report(report):
+        return True
+    for i in range(len(report)):
+        temp = report[:i] + report[i+1:]
+        if check_report(temp):
+            return True
+    return False
+
+def check_report(report):
+    if len(report) < 2:
+        return False
+    status = False
+    if report[0] < report[1]:
+        typ = 1
+    elif report[0] > report[1]:
+        typ = -1
+    else:
+        return False
+    
+    prev_num = None
+    for num in report:
+        if prev_num is not None:
+            if 1 <= abs(num-prev_num) <= 3:
+                if num < prev_num and typ == -1:
+                    status = True
+                elif num > prev_num and typ == 1:
+                    status = True
+                else:
+                    return False
+            else:
+                return False
+        prev_num = num
+    return status
+
 def do_challenge():
     with open('input.txt') as f:
         lines = f.readlines()
@@ -8,45 +53,8 @@ def do_challenge():
         for num in line.strip().split():
             report.append(int(num))
         reports.append(report)
-    
-    safe = 0
 
-    for ind, report in enumerate(reports):
-        status = False
-        if report[0] < report[1]:
-            typ = 1
-            #print('Accending')
-        elif report[0] > report[1]:
-            typ = -1
-            #print('Decending')
-        else:
-            #print('First two are the same {} and {}'.format(report[0], report[1]))
-            continue
-        
-        prev_num = None
-        for num in report:
-            if prev_num is not None:
-                if 1 <= abs(num-prev_num) <= 3:
-                    #print('\tDifference is {} type is {}'.format(abs(num-prev_num), typ))
-                    if num < prev_num and typ == -1:
-                        #print('\t\tValid Decending')
-                        status = True
-                    elif num > prev_num and typ == 1:
-                        #print('\t\tValid Accending')
-                        status = True
-                    else:
-                        status = False
-                        #print('\t----------------Not Valid---------------')
-                        break
-                else:
-                    status = False
-                    #print('\t----------------Not Valid---------------')
-                    break
-            prev_num = num
-        #print('REPORT ENDED {}\n\n'.format(ind))
-        
-        if status:
-            safe += 1
+    safe, nots = report_check(reports)
 
     return safe
 
