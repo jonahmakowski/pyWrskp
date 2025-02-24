b66b6acf8b9614462145b4f589f91e4bf2791392
jonahmakowski <jonah@makowski.ca>
2025-02-24 12:12:41 -0500

    Added docs to this file

--- src/adventOfCode/day6/puzzle1.py
+++ src/adventOfCode/day6/puzzle1.py
@@ -1,6 +1,18 @@
 import pyWrkspPackage
 
 def find_gaurd(data):
+    """
+    Finds the coordinates of the guard in the given 2D list.
+
+    Args:
+        data (list of list of str): A 2D list representing the grid where each element is a string.
+
+    Returns:
+        tuple: A tuple (x, y) representing the coordinates of the guard.
+
+    Raises:
+        Exception: If no guard is found in the grid.
+    """
     for y, row in enumerate(data):
         for x, item in enumerate(row):
             if item == "^":
@@ -8,6 +20,19 @@ def find_gaurd(data):
     raise Exception("No gaurd found")
 
 def mainloop(data: list, direc: tuple):
+    """
+    Executes the main loop logic for processing the data based on the given direction.
+
+    Args:
+        data (list): A 2D list representing the grid or map.
+        direc (tuple): A tuple representing the current direction (dirx, diry).
+
+    Returns:
+        tuple: A tuple containing the updated data and direction.
+
+    Raises:
+        Exception: If the data contains an invalid character.
+    """
     x, y = find_gaurd(data)
     dirx, diry = direc
     if data[y+diry][x+dirx] == "." or data[y+diry][x+dirx] == "X":
@@ -27,6 +52,15 @@ def mainloop(data: list, direc: tuple):
     raise Exception("Invalid data")
 
 def count(data):
+    """
+    Counts the number of occurrences of the character 'X' in a 2D list.
+
+    Args:
+        data (list of list of str): A 2D list where each element is a string.
+
+    Returns:
+        int: The count of 'X' characters found in the 2D list.
+    """
     value = 1
     for row in data:
         for item in row:
@@ -35,6 +69,20 @@ def count(data):
     return value
 
 def main():
+    """
+    Main function to execute the puzzle logic.
+    This function performs the following steps:
+    1. Loads data from the file "input.txt" using the pyWrkspPackage.
+    2. Splits the data into lines and converts each line into a list of characters.
+    3. Prints the initial state of the data.
+    4. Calls the mainloop function to process the data and update the direction.
+    5. Prints the updated state of the data.
+    6. Continuously calls the mainloop function to process the data until an IndexError occurs.
+    7. Prints the updated state of the data after each iteration.
+    8. Returns the count of a specific condition in the data.
+    Returns:
+        int: The count of a specific condition in the data.
+    """
     data = pyWrkspPackage.load_from_file("input.txt").split("\n")
     data = [list(item) for item in data]
     for point in data: print(*point)
