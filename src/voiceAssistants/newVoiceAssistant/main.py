bb59527638cae9c36843ff587bc123346d3583a1
jonahmakowski <jonah@makowski.ca>
2025-03-18 22:25:51 -0400

    Allowed the assistant to know the date and time
    The assistant now also knows the user's name, as configured at the top of the file.

--- src/voiceAssistants/newVoiceAssistant/main.py
+++ src/voiceAssistants/newVoiceAssistant/main.py
@@ -8,6 +8,7 @@ from time import sleep
 import pyttsx3
 import actions
 import speech_recognition as sr
+from datetime import datetime
 
 # General Setup
 load_dotenv()
@@ -21,6 +22,7 @@ COMMANDS = {'open-app': [actions.open_app, True], 'search-the-web': [actions.sea
             'pause-music': [actions.pause, False], 'open-webpage': [actions.open_webpage, True],
             'open-folder': [actions.open_directory_in_finder, True], 'hide-application': [actions.hide_app, True],
             'question-mode': ['Question Mode', False]}
+USER_NAME = 'Jonah'
 
 # Setting up Audio Systems
 mixer.init() # mp3 player init
@@ -30,17 +32,18 @@ engine.setProperty('volume', 1)  # Volume level (0.0 to 1.0)
 
 # Confirming env variables are set
 if AI_KEY is None or VOICE_KEY is None:
-    raise ValueError('An environment variable (AI_KEY or ) is not set')
+    raise ValueError('An environment variable (AI_KEY or VOICE_DETECTION_TOKEN) is not set')
 
 # Define Functions
-def play_sound(hold: bool) -> None:
+def play_sound(hold: bool, sound='audio.mp3') -> None:
     """
-    Play a sound from 'audio.mp3'.
+    Play a sound file using the mixer module.
 
     Args:
         hold (bool): If True, the function will block until the sound finishes playing.
+        sound (str, optional): The path to the sound file to play. Defaults to 'audio.mp3'.
     """
-    mixer.music.load('audio.mp3')
+    mixer.music.load(sound)
     mixer.music.play()
     if hold:
         while mixer.music.get_busy():
@@ -80,11 +83,15 @@ def get_message_list(cur_list: list, message: str, max_size=10) -> list:
     Returns:
         list: The updated message list.
     """
+    current_datetime = datetime.now()
+    cur_date = current_datetime.strftime("%B %d, %Y")
+    cur_time = current_datetime.strftime("%I:%M %p")
     if not cur_list:
-        return [{"role": "system", "content": SYS_PROMPT}, {"role": "user", "content": message}]
+        return [{"role": "system", "content": SYS_PROMPT.format(cur_date, cur_time, USER_NAME)},
+                {"role": "user", "content": message}]
     while len(cur_list) > max_size:
         cur_list.pop(1)
-    cur_list[0] = {"role": "system", "content": SYS_PROMPT}
+    cur_list[0] = {"role": "system", "content": SYS_PROMPT.format(cur_date, cur_time, USER_NAME)}
     cur_list.append({"role": "user", "content": message})
     return cur_list
 
@@ -132,6 +139,8 @@ def main():
     recoder = PvRecorder(device_index=-1, frame_length=porcupine.frame_length)
     message_list = []
     question_mode = False
+
+    play_sound(True, 'bootup.mp3')
 
     try:
         recoder.start()
