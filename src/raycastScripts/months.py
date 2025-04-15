c68f4b040b9f00f507683d1dd384fc9bc1be0476
jonahmakowski <jonah@makowski.ca>
2025-04-14 20:37:24 -0400

    Added this b/c I'm an idiot

---
+++ src/raycastScripts/months.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Months
+# @raycast.mode fullOutput
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName Helper
+
+# Documentation:
+# @raycast.description Prints out a list of months
+# @raycast.author Jonah
+# @raycast.authorURL N/A
+
+print('''1.\tJanuary
+2.\tFebruary
+3.\tMarch
+4.\tApril
+5.\tMay
+6.\tJune
+7.\tJuly
+8.\tAugust
+9.\tSeptember
+10.\tOctober
+11.\tNovember
+12.\tDecember''')
+
