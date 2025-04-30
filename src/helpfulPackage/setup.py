2be8d5739dd2337fbd31ff6e9c14af001871e231
jonahmakowski <jonah@makowski.ca>
2025-04-30 11:28:38 -0400

    Bumbed version number to reflect the previous changes

--- src/helpfulPackage/setup.py
+++ src/helpfulPackage/setup.py
@@ -5,7 +5,7 @@ with open("app/README.md", "r") as f:
 
 setup(
     name="pyWrkspPackage",
-    version="0.4.6",
+    version="0.5.0",
     description="A group of helpful functions for Python",
     package_dir={"": "app"},
     packages=find_packages(where="app"),
