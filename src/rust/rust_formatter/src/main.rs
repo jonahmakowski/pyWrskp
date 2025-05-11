4f3779251ff788bf2a0f467803cebdb6a7b6e249
jonahmakowski <jonah@makowski.ca>
2025-05-11 17:28:08 -0400

    Realized that was already there

--- src/rust/rust_formatter/src/main.rs
+++ src/rust/rust_formatter/src/main.rs
@@ -6,7 +6,6 @@ fn main() {
         if entry.path().to_string_lossy().contains("Cargo.toml") {
             let folder = entry.path().parent().unwrap();
             Command::new("cargo")
-                .current_dir(folder)
                 .arg("fmt")
                 .current_dir(folder)
                 .status()
