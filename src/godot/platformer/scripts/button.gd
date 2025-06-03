b82d2c7b8310055c7fdddb732410b1a663041222
jonahmakowski <jonah@makowski.ca>
2025-06-03 16:37:48 -0400

    About to update textures

--- src/godot/platformer/scripts/button.gd
+++ src/godot/platformer/scripts/button.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	$AnimatedSprite2D.animation = "pressed"
+	print('I got pressed')
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	$AnimatedSprite2D.animation = "unpressed"
+	print('I got unpressed')
