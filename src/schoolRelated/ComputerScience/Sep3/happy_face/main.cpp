d6733e6daf3b9927d859061c880da44872cf332e
jonahmakowski <jonah@makowski.ca>
2025-09-04 12:35:13 -0400

    Add C++ school assignments and expand .gitignore
    Introduces two simple C++ programs, "hello_world" and "happy_face", as part of computer science coursework.
    
    Updates the .gitignore file to exclude common C/C++ build artifacts, such as executables, object files, and debug symbols. Also adds an ignore rule for the .vscode directory.

---
+++ src/schoolRelated/ComputerScience/Sep3/happy_face/main.cpp
@@ -0,0 +1,13 @@
+/* Jonah Makowski - September 3, 2025 - Hello World and Happy Face
+  Prints hello world and makes a smiley face */
+#include <stdio.h>
+
+int main(){
+  printf("Hello World\n\n");
+  
+  printf("/-----\\ \n");
+  printf("| . . |  \n");
+  printf("|     |  \n");
+  printf("| \\_/ | \n");
+  printf("\\_____/ \n");
+}
---
+++ src/schoolRelated/ComputerScience/Sep3/hello_world/main.cpp
@@ -0,0 +1,10 @@
+/*Jonah Makowski - September 3, 2025 - Hello World Program
+  Prints "Hello world" to console*/
+
+#include <stdio.h> // Imports standard input/output library
+
+// The int in the following line means that it returns an integer
+int main(){ // Main function, is always executed first
+    printf("Hello world.\n"); // Print function, the f stands for format
+    return 0; // return 0 to indicate success
+}
