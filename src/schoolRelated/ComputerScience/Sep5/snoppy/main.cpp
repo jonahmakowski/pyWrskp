e44759292496ca7229c45706db0841b995ce4056
jonahmakowski <jonah@makowski.ca>
2025-09-07 17:46:40 -0400

    Adds BMI calculator program
    Implements a program that collects user age, height, and weight
    Displays collected information in a formatted table
    Removes placeholder main.cpp file and replaces with functional code

--- src/schoolRelated/ComputerScience/Sep5/snoppy/main.cpp
+++ src/schoolRelated/ComputerScience/Sep5/snoppy/main.cpp
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
 int main() {
+    int age;
+    int height;
     int weight;
+
+    printf("How old are you?\n");
+    scanf("%d", &age);
+
+    printf("How tall are you (in cm)?\n");
+    scanf("%d", &height);
+    
+    printf("How much do you weigh (in kg)?\n");
+    scanf("%d", &weight);
+    
+    printf("Age\tHeight(cm)\tWeight(kg)\n");
+    printf("---\t----------\t----------\n");
+    printf("%3d\t%10d\t%10d\n", age, height, weight);
 }
