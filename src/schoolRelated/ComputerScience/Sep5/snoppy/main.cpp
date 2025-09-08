9d285a49dcde73f0a1056916c37be490aed52e60
jonahmakowski <jonah@makowski.ca>
2025-09-08 12:47:59 -0400

    Adds BMI calculation and categorization
    Implements BMI calculation from user-provided height and weight
    Adds BMI categorization (Underweight, Normal, Overweight, Obese)
    Updates output to display BMI and category alongside age, height, and weight

--- src/schoolRelated/ComputerScience/Sep5/snoppy/main.cpp
+++ src/schoolRelated/ComputerScience/Sep5/snoppy/main.cpp
@@ -1,3 +1,8 @@
+/*
+Jonah Makowski - Sep 5-8, 2024
+This program gets the age, height and weight from the user, calculates the BMI, prints all the data, and categorizes the BMI.
+*/
+
 #include <stdio.h>
 
 int main() {
@@ -5,16 +10,33 @@ int main() {
     int height;
     int weight;
 
+    // Get the age
     printf("How old are you?\n");
     scanf("%d", &age);
 
+    // Get the height in cm
     printf("How tall are you (in cm)?\n");
     scanf("%d", &height);
     
+    // Get the weight in kg
     printf("How much do you weigh (in kg)?\n");
     scanf("%d", &weight);
     
-    printf("Age\tHeight(cm)\tWeight(kg)\n");
-    printf("---\t----------\t----------\n");
-    printf("%3d\t%10d\t%10d\n", age, height, weight);
+    float bmi = weight / ((height / 100) ^ 2); // Calculate bmi weight / height in m squared
+
+    // Print everything except the bmi category
+    printf("Age\tHeight(cm)\tWeight(kg)\tBMI\t\tCategory\n");
+    printf("---\t----------\t----------\t--------\t-----------\n");
+    printf("%3d\t%10d\t%10d\t%8.2f\t", age, height, weight, bmi);
+
+    // Choose the bmi using if/then (selection) statements
+    if (bmi <= 18.4) {
+        printf("Underweight\n");
+    } else if ( 18.5 <= bmi && bmi <= 24.9) {
+        printf("Normal\n");
+    } else if (25.0 <= bmi && bmi <= 39.9) {
+        printf("Overweight\n");
+    } else {
+        printf("Obese\n");
+    }
 }
