b11fd79244408238ab686df32e237910df0bffca
jonahmakowski <jonah@makowski.ca>
2025-09-08 13:13:21 -0400

    Fixes BMI calculation and weight input
    Changes weight variable type from int to float for more accurate BMI calculations
    Corrects BMI formula from using ^ (bitwise XOR) to * (multiplication)
    Updates weight display format to show two decimal places
    Removes debug-related binary file changes

--- src/schoolRelated/ComputerScience/Sep5/snoppy/main.cpp
+++ src/schoolRelated/ComputerScience/Sep5/snoppy/main.cpp
@@ -8,7 +8,7 @@ This program gets the age, height and weight from the user, calculates the BMI, 
 int main() {
     int age;
     int height;
-    int weight;
+    float weight;
 
     // Get the age
     printf("How old are you?\n");
@@ -20,14 +20,14 @@ int main() {
     
     // Get the weight in kg
     printf("How much do you weigh (in kg)?\n");
-    scanf("%d", &weight);
+    scanf("%f", &weight);
     
-    float bmi = weight / ((height / 100) ^ 2); // Calculate bmi weight / height in m squared
+    float bmi = weight / ((height / 100) * (height / 100)); // Calculate bmi weight / height in m squared
 
     // Print everything except the bmi category
     printf("Age\tHeight(cm)\tWeight(kg)\tBMI\t\tCategory\n");
     printf("---\t----------\t----------\t--------\t-----------\n");
-    printf("%3d\t%10d\t%10d\t%8.2f\t", age, height, weight, bmi);
+    printf("%3d\t%10d\t%10.2f\t%8.2f\t", age, height, weight, bmi);
 
     // Choose the bmi using if/then (selection) statements
     if (bmi <= 18.4) {
