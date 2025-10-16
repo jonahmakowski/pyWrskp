7d34da0dcce202e02243360a8f50e8e7405c3610
jonahmakowski <jonah@makowski.ca>
2025-10-16 12:55:02 -0400

    Improves laptop data processing flexibility
    Replaces fixed-size array iteration with dynamic file reading
    Removes hardcoded laptop count limit for better scalability
    Allows processing any number of entries in the input file
    
    Updates comment to clarify development environment

--- src/schoolRelated/ComputerScience/Oct14/hardwareproject/main.cpp
+++ src/schoolRelated/ComputerScience/Oct14/hardwareproject/main.cpp
@@ -1,10 +1,11 @@
 // Jonah Makowski - Display stats about laptops from different universities
+// I did this is vscode on my computer, that's why it might look copy pasted - becuse it was
 #include <stdio.h>
 #include <string.h>
 
 int main() {
     // Define how many laptops are in the file
-    const int MAX_LAPTOPS = 5;
+    //const int MAX_LAPTOPS = 5;
 
     // Define variables to hold the data
     char line[200];
@@ -27,10 +28,9 @@ int main() {
     // Process and display the data
     printf("University\t\tBrand\t\tRAM in GB\tStorage in GB\t\tPrice\t\tPrice per GB of RAM\tPrice per GB of Storage\n");
     printf("-----------------------------------------------------------------------------------------------------------------------------------------------\n");
-    for (int i = 0; i < MAX_LAPTOPS; i++) {
-        // Read the line from the file
-        fgets(line, sizeof(line), fptr);
-
+    
+    // Read the line from the file
+    while (fgets(line, sizeof(line), fptr)) {
         // Parse the line
         sscanf(line, "%49[0-9a-zA-Z ]\t%s %d %d %f", university, brand, &ram, &storage, &price);
         
