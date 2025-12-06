367e418de18b370f6a194ffb492ec90490191ce2
jonahmakowski <jonah@makowski.ca>
2025-12-06 09:52:09 -0500

    Implement minigrep functionality with search and case-insensitive search features

---
+++ src/rust/minigrep/src/lib.rs
@@ -0,0 +1,49 @@
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(&query) {
+            result.push(line);
+        }
+    }
+
+    result
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+
+    let mut result = Vec::new();
+    
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            result.push(line);
+        }
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "Rust:\nsafe, fast, productive\nPick three.\nDuct Tape.";
+
+        assert_eq!(vec!["safe, fast, productive"], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "RuST";
+        let contents = "Rust:\nsafe, fast, productive\nPick three\nTrust Me";
+
+        assert_eq!(
+            vec!["Rust:", "Trust Me"],
+            search_case_insensitive(query, contents),
+        )
+    }
+}
---
+++ src/rust/minigrep/src/main.rs
@@ -0,0 +1,54 @@
+use std::{env, fs, process, error::Error};
+use minigrep::{search, search_case_insensitive};
+
+fn main() {
+    let conf = Config::build().unwrap_or_else(|err| {
+        eprintln!("{err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = run(conf) {
+        eprintln!("Error: {e}");
+        process::exit(1);
+    }
+}
+
+struct Config {
+    query: String,
+    file: String,
+    ignore_case: bool,
+}
+
+impl Config {
+    fn build() -> Result<Self, &'static str> {
+        let args: Vec<String> = env::args().collect();
+
+        if args.len() < 3 {
+            return Err("Not enough arguments, be sure to include a file and a search string");
+        }
+        
+        let conf = Config {
+            query: args[1].clone(),
+            file: args[2].clone(),
+            ignore_case: env::var("IGNORE_CASE").is_ok(),
+        };
+
+        Ok(conf)
+    }
+}
+
+fn run(conf: Config) -> Result<(), Box<dyn Error>> {
+    let file_contents = fs::read_to_string(&conf.file)?;
+
+    let results = if conf.ignore_case {
+        search_case_insensitive(&conf.query, &file_contents)
+    } else {
+        search(&conf.query, &file_contents)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
+
+    Ok(())
+}
