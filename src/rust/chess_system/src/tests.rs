020e52300a5f58b69bb11dc6975af9764428f2cd
jonahmakowski <jonah@makowski.ca>
2025-12-08 09:19:58 -0500

    Add tests for board empty position and location retrieval

--- src/rust/chess_system/src/tests.rs
+++ src/rust/chess_system/src/tests.rs
@@ -25,3 +25,26 @@ fn move_piece() {
 
     assert_eq!(format!("{new_board}"), correct_output);
 }
+
+#[test]
+fn board_empty_position() {
+    let new_board = types::Board::new();
+
+    assert!(!new_board.is_location_empty(&types::Position {x: 0, y: 0}));
+    assert!(new_board.is_location_empty(&types::Position {x: 3, y: 3}));
+}
+
+#[test]
+fn board_location() {
+    let new_board = types::Board::new();
+
+    assert_eq!(
+        new_board.at_location(&types::Position { x: 0, y: 0 }),
+        Some(types::ChessPiece::new(types::PieceType::ROOK, types::Side::WHITE, types::Position {x: 0, y: 0}))
+    );
+
+    assert_eq!(
+        new_board.at_location(&types::Position { x: 3, y: 3 }),
+        None
+    );
+}
--- src/rust/chess_system/src/types.rs
+++ src/rust/chess_system/src/types.rs
@@ -1,12 +1,12 @@
 use std::fmt;
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq, Debug)]
 pub enum Side {
     BLACK,
     WHITE,
 }
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq, Debug)]
 pub enum PieceType {
     PAWN,
     ROOK,
@@ -16,7 +16,7 @@ pub enum PieceType {
     KING,
 }
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq, Debug)]
 pub struct ChessPiece {
     t: PieceType,
     color: Side,
@@ -24,10 +24,110 @@ pub struct ChessPiece {
     symbol: char,
 }
 
-#[derive(Clone, PartialEq)]
+impl ChessPiece {
+    pub fn new(t: PieceType, color: Side, position: Position) -> Self {
+        let symbol = match (&t, &color) {
+            (PieceType::PAWN, Side::WHITE) => '♙',
+            (PieceType::PAWN, Side::BLACK) => '♟',
+            (PieceType::ROOK, Side::WHITE) => '♖',
+            (PieceType::ROOK, Side::BLACK) => '♜',
+            (PieceType::KNIGHT, Side::WHITE) => '♘',
+            (PieceType::KNIGHT, Side::BLACK) => '♞',
+            (PieceType::BISHOP, Side::WHITE) => '♗',
+            (PieceType::BISHOP, Side::BLACK) => '♝',
+            (PieceType::QUEEN, Side::WHITE) => '♕',
+            (PieceType::QUEEN, Side::BLACK) => '♛',
+            (PieceType::KING, Side::WHITE) => '♔',
+            (PieceType::KING, Side::BLACK) => '♚',
+        };
+
+        Self {
+            t,
+            color,
+            position,
+            symbol,
+        }
+    }
+
+    pub fn is_valid_move(&self, new_position: Position, board: Board) -> bool {
+        match self.t {
+            PieceType::PAWN => {
+                if new_position.x < 0 || new_position.y < 0 || new_position.x > 7 || new_position.y > 7 {
+                    return false;
+                }
+
+                let (start_row, direction) = if self.color == Side::WHITE {(1, 1)} else {(6, -1)};
+                let forward_one = self.position.add(Position {x: 0, y: 1*direction});
+                let forward_two = self.position.add(Position {x: 0, y: 2*direction});
+                let diagonal_1 = self.position.add(Position {x: 1, y: 1*direction});
+                let diagonal_2 = self.position.add(Position {x: -1, y: 1*direction});
+
+                let diagonal_one_available = {
+                    match board.at_location(&diagonal_1) {
+                        Some(obj) => {
+                            if obj.color != self.color {
+                                true
+                            } else {
+                                false
+                            }
+                        }
+                        None => false
+                    }
+                };
+
+                let diagonal_two_available = {
+                    match board.at_location(&diagonal_2) {
+                        Some(obj) => {
+                            if obj.color != self.color {
+                                true
+                            } else {
+                                false
+                            }
+                        }
+                        None => false
+                    }
+                };
+
+                if new_position == forward_one && board.is_location_empty(&forward_one) {
+                    return true;
+                } else if self.position.y == start_row && new_position == forward_two && board.is_location_empty(&forward_one) && board.is_location_empty(&forward_two) {
+                    return true;
+                } else if new_position == diagonal_1 && diagonal_one_available {
+                    return true;
+                } else if new_position == diagonal_2 && diagonal_two_available {
+                    return true;
+                } else {
+                    return false;
+                }
+            },
+            PieceType::ROOK => panic!(),
+            PieceType::KNIGHT => panic!(),
+            PieceType::BISHOP => panic!(),
+            PieceType::QUEEN => panic!(),
+            PieceType::KING => panic!(),
+        }
+    }
+}
+
+impl fmt::Display for ChessPiece {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.symbol)
+    }
+}
+
+#[derive(Clone, PartialEq, Debug)]
 pub struct Position {
-    pub x: u8,
-    pub y: u8,
+    pub x: i8,
+    pub y: i8,
+}
+
+impl Position {
+    pub fn add(&self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
 }
 
 pub struct Board {
@@ -41,123 +141,41 @@ impl Board {
 
         // White Pawns
         for x in 0..8 {
-            pieces.push(ChessPiece {
-                t: PieceType::PAWN,
-                color: Side::WHITE,
-                position: Position { x, y: 1 },
-                symbol: '♙',
-            });
+            pieces.push(ChessPiece::new(
+                PieceType::PAWN,
+                Side::WHITE,
+                Position { x, y: 1 },
+            ));
         }
 
         // Black Pawns
         for x in 0..8 {
-            pieces.push(ChessPiece {
-                t: PieceType::PAWN,
-                color: Side::BLACK,
-                position: Position { x, y: 6 },
-                symbol: '♟',
-            });
+            pieces.push(ChessPiece::new(
+                PieceType::PAWN,
+                Side::BLACK,
+                Position { x, y: 6 },
+            ));
         }
 
         // White Main Row
-        pieces.push(ChessPiece {
-            t: PieceType::ROOK,
-            color: Side::WHITE,
-            position: Position { x: 0, y: 0 },
-            symbol: '♖',
-        });
-        pieces.push(ChessPiece {
-            t: PieceType::KNIGHT,
-            color: Side::WHITE,
-            position: Position { x: 1, y: 0 },
-            symbol: '♘',
-        });
-        pieces.push(ChessPiece {
-            t: PieceType::BISHOP,
-            color: Side::WHITE,
-            position: Position { x: 2, y: 0 },
-            symbol: '♗',
-        });
-        pieces.push(ChessPiece {
-            t: PieceType::QUEEN,
-            color: Side::WHITE,
-            position: Position { x: 3, y: 0 },
-            symbol: '♕',
-        });
-        pieces.push(ChessPiece {
-            t: PieceType::KING,
-            color: Side::WHITE,
-            position: Position { x: 4, y: 0 },
-            symbol: '♔',
-        });
-        pieces.push(ChessPiece {
-            t: PieceType::BISHOP,
-            color: Side::WHITE,
-            position: Position { x: 5, y: 0 },
-            symbol: '♗',
-        });
-        pieces.push(ChessPiece {
-            t: PieceType::KNIGHT,
-            color: Side::WHITE,
-            position: Position { x: 6, y: 0 },
-            symbol: '♘',
-        });
-        pieces.push(ChessPiece {
-            t: PieceType::ROOK,
-            color: Side::WHITE,
-            position: Position { x: 7, y: 0 },
-            symbol: '♖',
-        });
+        pieces.push(ChessPiece::new(PieceType::ROOK, Side::WHITE, Position { x: 0, y: 0 }));
+        pieces.push(ChessPiece::new(PieceType::KNIGHT, Side::WHITE, Position { x: 1, y: 0 }));
+        pieces.push(ChessPiece::new(PieceType::BISHOP, Side::WHITE, Position { x: 2, y: 0 }));
+        pieces.push(ChessPiece::new(PieceType::QUEEN, Side::WHITE, Position { x: 3, y: 0 }));
+        pieces.push(ChessPiece::new(PieceType::KING, Side::WHITE, Position { x: 4, y: 0 }));
+        pieces.push(ChessPiece::new(PieceType::BISHOP, Side::WHITE, Position { x: 5, y: 0 }));
+        pieces.push(ChessPiece::new(PieceType::KNIGHT, Side::WHITE, Position { x: 6, y: 0 }));
+        pieces.push(ChessPiece::new(PieceType::ROOK, Side::WHITE, Position { x: 7, y: 0 }));
 
         // Black Main Row
-        pieces.push(ChessPiece {
-            t: PieceType::ROOK,
-            color: Side::BLACK,
-            position: Position { x: 0, y: 7 },
-            symbol: '♜',
-        });
-        pieces.push(ChessPiece {
-            t: PieceType::KNIGHT,
-            color: Side::BLACK,
-            position: Position { x: 1, y: 7 },
-            symbol: '♞',
-        });
-        pieces.push(ChessPiece {
-            t: PieceType::BISHOP,
-            color: Side::BLACK,
-            position: Position { x: 2, y: 7 },
-            symbol: '♝',
-        });
-        pieces.push(ChessPiece {
-            t: PieceType::QUEEN,
-            color: Side::BLACK,
-            position: Position { x: 3, y: 7 },
-            symbol: '♛',
-        });
-        pieces.push(ChessPiece {
-            t: PieceType::KING,
-            color: Side::BLACK,
-            position: Position { x: 4, y: 7 },
-            symbol: '♚',
-        });
-        pieces.push(ChessPiece {
-            t: PieceType::BISHOP,
-            color: Side::BLACK,
-            position: Position { x: 5, y: 7 },
-            symbol: '♝',
-        });
-        pieces.push(ChessPiece {
-            t: PieceType::KNIGHT,
-            color: Side::BLACK,
-            position: Position { x: 6, y: 7 },
-            symbol: '♞',
-        });
-        pieces.push(ChessPiece {
-            t: PieceType::ROOK,
-            color: Side::BLACK,
-            position: Position { x: 7, y: 7 },
-            symbol: '♜',
-        });
+        pieces.push(ChessPiece::new(PieceType::ROOK, Side::BLACK, Position { x: 0, y: 7 }));
+        pieces.push(ChessPiece::new(PieceType::KNIGHT, Side::BLACK, Position { x: 1, y: 7 }));
+        pieces.push(ChessPiece::new(PieceType::BISHOP, Side::BLACK, Position { x: 2, y: 7 }));
+        pieces.push(ChessPiece::new(PieceType::QUEEN, Side::BLACK, Position { x: 3, y: 7 }));
+        pieces.push(ChessPiece::new(PieceType::KING, Side::BLACK, Position { x: 4, y: 7 }));
+        pieces.push(ChessPiece::new(PieceType::BISHOP, Side::BLACK, Position { x: 5, y: 7 }));
+        pieces.push(ChessPiece::new(PieceType::KNIGHT, Side::BLACK, Position { x: 6, y: 7 }));
+        pieces.push(ChessPiece::new(PieceType::ROOK, Side::BLACK, Position { x: 7, y: 7 }));
 
         Self {
             pieces,
@@ -187,6 +205,8 @@ impl Board {
             return Err("Positions are outside of bounds");
         }
 
+        self.pieces.retain(|piece| piece.position != pos_to);
+
         for piece in self.pieces.iter_mut() {
             if piece.position == pos_from {
                 piece.position = pos_to.clone();
@@ -195,6 +215,22 @@ impl Board {
         }
 
         Err("Can't find piece at that position")
+    }
+
+    pub fn at_location(&self, pos: &Position) -> Option<ChessPiece> {
+        let grid_version = self.into_grid();
+
+        match &grid_version[pos.y as usize][pos.x as usize] {
+            Some(obj) => Some(obj.clone()),
+            None => None,
+        }
+    }
+
+    pub fn is_location_empty(&self, pos: &Position) -> bool {
+        match self.at_location(&pos) {
+            Some(_) => return false,
+            None => return true,
+        }
     }
 }
 
